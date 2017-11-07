8f32884fe706356b29e4674dd83703966478c999
Alexandre De Pablo <alexandre.depablo@gmail.com>
2017-11-07 18:49:28 +0100

    Refactor : use Reform for contact's validation and creation

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,12 +1,3 @@
 class Contact < ApplicationRecord
   belongs_to :user
-
-  validates :email, presence: true,
-    format: { with: /\A[a-zA-Z0-9_.+\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-.]+\z/ }
-
-  validates :phone_number, format: { with: /\A0\d(\d{2}){4}$\z/ },
-    allow_blank: true
-
-  validates :contact_type, presence: true,
-    inclusion: { in: %w(admin tech other) }
 end
