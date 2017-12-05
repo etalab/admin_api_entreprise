546f6f4a7558c472813420509a703d5186f662a5
Alexandre De Pablo <alexandre.depablo@gmail.com>
2017-12-05 14:25:52 +0100

    Boyscoot : remove old use of strong parameters

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,24 +33,4 @@ class UsersController < ApplicationController
       render json: {}, status: 404
     end
   end
-
-  private
-
-  def create_params
-    @create_params ||= params.permit(:email, :context, :user_type, contacts: [:email, :phone_number, :contact_type], token_payload: [])
-  end
-
-  def all_params_for_create?
-    mandatory_params = %i(email context user_type)
-    mandatory_params.all? { |e| create_params.has_key? e }
-  end
-
-  def update_params
-    params.permit :email, :context
-  end
-
-  def has_updating_params?
-    updating_params = %i(email context)
-    updating_params.any? { |p| update_params.has_key? p }
-  end
 end
