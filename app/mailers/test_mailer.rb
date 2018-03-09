876c6559d2abc34cbcb0eebaec0ddc5e63544818
Alexandre De Pablo <alexandre.depablo@gmail.com>
2018-03-09 13:14:40 +0100

    Remove test mailers

--- app/mailers/test_mailer.rb
+++
@@ -1,8 +0,0 @@
-class TestMailer < ApplicationMailer
-  default from: 'brindu@entreprise.api.gouv.fr'
-  layout 'mailer'
-
-  def test
-    mail(to: 'tech@entreprise.api.gouv.fr', subject: 'Yeah Admin rocks!')
-  end
-end
