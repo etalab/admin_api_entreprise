ef65bd4d34f191e57acc3527e542ee4a0c6ad438
Alexis Leclerc <alexis@alxs.fr>
2019-03-27 19:10:02 +0200

    [Mail] add confirm account notice & rename

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,11 +2,21 @@ class UserMailer < ApplicationMailer
   default from: 'no-reply@entreprise.api.gouv.fr'
   layout 'mailer'
 
-  def confirmation_request(user)
+  def confirm_account_action(user)
     subject = 'API Entreprise - Activation de compte utilisateur'
 
     @user = user
     @confirmation_url = Rails.configuration.account_confirmation_url.to_s + "?confirmation_token=#{@user.confirmation_token}"
     mail(to: @user.email, subject: subject)
+  end
+
+  def confirm_account_notice(user)
+    @user = user
+    subject = 'API Entreprise - Activation de compte utilisateur'
+
+    recipients = user.contacts.pluck(:email).uniq
+    recipients.delete user.email
+
+    mail(to: recipients, subject: subject)
   end
 end
