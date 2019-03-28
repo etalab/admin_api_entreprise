688fa4da724466b4d8e4bdde05c13a03b1167a1f
Alexis Leclerc <alexis@alxs.fr>
2019-03-28 13:06:53 +0200

    Fix failing spec, add spec & refactor spec
      fix spec by addind ClassDependencies and back to self[:roles_seed]
      add spec to test default value of roles_seed
      refactor spec: do not use described class to test itself

--- spec/concepts/role/operation/db_seed_spec.rb
+++ spec/concepts/role/operation/db_seed_spec.rb
@@ -1,38 +1,50 @@
 require 'rails_helper'
 
 describe Role::DBSeed do
-  let(:roles_seed) do
-    [
-      { name: 'Role 1', code: 'rol1' },
-      { name: 'Role 2', code: 'rol2' }
-    ]
+  describe 'with custom roles seed' do
+    subject { described_class.call roles_seed: roles_seed }
+
+    let(:roles_seed) do
+      [
+        { name: 'Role 1', code: 'rol1' },
+        { name: 'Role 2', code: 'rol2' }
+      ]
+    end
+    let(:log) { subject[:log] }
+
+    context 'when roles do not exist' do
+      it 'saves them into db' do
+        expect { subject }.to change(Role, :count).by(2)
+      end
+
+      it 'logs role\'s creation' do
+        expect(log).to include('Role created : name "Role 1", code "rol1"')
+        expect(log).to include('Role created : name "Role 2", code "rol2"')
+      end
+    end
+
+    context 'when roles already exist in database' do
+      before do
+        create :role, name: 'Role 1', code: 'rol1'
+        create :role, name: 'Role 2', code: 'rol2'
+      end
+
+      it 'does not saves the roles' do
+        expect { subject }.to_not change(Role, :count)
+      end
+
+      it 'logs the error' do
+        expect(log).to include('Warning role already exists : name "Role 1", code "rol1"')
+        expect(log).to include('Warning role already exists : name "Role 2", code "rol2"')
+      end
+    end
   end
-  let(:result) { described_class.call(params: {}, roles_seed: roles_seed) }
-  let(:log) { result[:log] }
 
-  context 'when roles do not exist' do
-    it 'saves them into db' do
-      expect { result }.to change(Role, :count).by(2)
-    end
+  describe 'roles_seed defaults to siade roles' do
+    subject { described_class.call }
 
-    it 'logs role\'s creation' do
-      expect(log).to include('Role created : name "Role 1", code "rol1"')
-      expect(log).to include('Role created : name "Role 2", code "rol2"')
-    end
-  end
-
-  context 'when roles already exist in database' do
-    before do
-      described_class.call(params: {}, roles_seed: roles_seed)
-    end
-
-    it 'does not saves the roles' do
-      expect { result }.to_not change(Role, :count)
-    end
-
-    it 'logs the error' do
-      expect(log).to include('Warning role already exists : name "Role 1", code "rol1"')
-      expect(log).to include('Warning role already exists : name "Role 2", code "rol2"')
+    it 'saves all roles in the database' do
+      expect { subject }.to change(Role, :count).by(18)
     end
   end
 end
