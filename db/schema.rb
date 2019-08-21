1924cff8d32e561a0448e6f1b135b592ba258faa
Alexandre De Pablo <alexandre.depablo@gmail.com>
2019-08-21 11:12:13 +0200

    Run migrations according to doorkeeper update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_08_074223) do
+ActiveRecord::Schema.define(version: 2019_08_21_082816) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -88,6 +88,7 @@ ActiveRecord::Schema.define(version: 2019_05_08_074223) do
     t.string "scopes", default: "", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "confidential", default: true, null: false
     t.index ["uid"], name: "index_oauth_applications_on_uid", unique: true
   end
 
