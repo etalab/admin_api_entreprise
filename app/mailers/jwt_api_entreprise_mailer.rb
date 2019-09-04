52e7966b26ada385d08e42fd9cc1ab0ecb115d75
Alexandre De Pablo <alexandre.depablo@gmail.com>
2019-09-04 16:25:58 +0200

    Move generic mailer configuration into ApplicationMailer class

--- app/mailers/jwt_api_entreprise_mailer.rb
+++ app/mailers/jwt_api_entreprise_mailer.rb
@@ -1,7 +1,4 @@
 class JwtApiEntrepriseMailer < ApplicationMailer
-  default from: 'tech@entreprise.api.gouv.fr'
-  layout 'mailer'
-
   def expiration_notice(jwt)
     mail(to: jwt.user.email)
   end
