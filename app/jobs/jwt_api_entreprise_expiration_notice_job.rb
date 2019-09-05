0444b6c9abff335e6dc10b67b8a179362b049c38
Alexandre De Pablo <alexandre.depablo@gmail.com>
2019-09-05 11:43:19 +0200

    Define periods to notifiy about JWT expiration

--- app/jobs/jwt_api_entreprise_expiration_notice_job.rb
+++ app/jobs/jwt_api_entreprise_expiration_notice_job.rb
@@ -3,5 +3,13 @@ class JwtApiEntrepriseExpirationNoticeJob < ApplicationJob
 
   def perform(*args)
     JwtApiEntreprise::Operation::NotifyExpiration.call(expire_in: 90)
+    JwtApiEntreprise::Operation::NotifyExpiration.call(expire_in: 60)
+    JwtApiEntreprise::Operation::NotifyExpiration.call(expire_in: 30)
+    JwtApiEntreprise::Operation::NotifyExpiration.call(expire_in: 14)
+    JwtApiEntreprise::Operation::NotifyExpiration.call(expire_in: 7)
+    JwtApiEntreprise::Operation::NotifyExpiration.call(expire_in: 5)
+    JwtApiEntreprise::Operation::NotifyExpiration.call(expire_in: 3)
+    JwtApiEntreprise::Operation::NotifyExpiration.call(expire_in: 2)
+    JwtApiEntreprise::Operation::NotifyExpiration.call(expire_in: 1)
   end
 end
--- spec/jobs/jwt_api_entreprise_expiration_notice_job_spec.rb
+++ spec/jobs/jwt_api_entreprise_expiration_notice_job_spec.rb
@@ -5,13 +5,22 @@ describe JwtApiEntrepriseExpirationNoticeJob do
 
   shared_examples 'sending expiration notices' do |nb_days|
     it "sends notifications for JWT expiration in #{nb_days} days" do
-      expect(JwtApiEntreprise::Operation::NotifyExpiration)
-        .to receive(:call)
-        .with(expire_in: nb_days)
+      notifier_spy = class_spy(JwtApiEntreprise::Operation::NotifyExpiration)
+      stub_const('JwtApiEntreprise::Operation::NotifyExpiration', notifier_spy)
 
       subject
+
+      expect(notifier_spy).to have_received(:call).with(expire_in: nb_days)
     end
   end
 
   it_behaves_like 'sending expiration notices', 90
+  it_behaves_like 'sending expiration notices', 60
+  it_behaves_like 'sending expiration notices', 30
+  it_behaves_like 'sending expiration notices', 14
+  it_behaves_like 'sending expiration notices', 7
+  it_behaves_like 'sending expiration notices', 5
+  it_behaves_like 'sending expiration notices', 3
+  it_behaves_like 'sending expiration notices', 2
+  it_behaves_like 'sending expiration notices', 1
 end
