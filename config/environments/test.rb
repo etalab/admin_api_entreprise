d8bf840040789230cb5475b457cf2d0351584b39
Alexandre De Pablo <alexandre.depablo@gmail.com>
2019-09-06 11:30:51 +0200

    Mock the Sidekiq adapter for ActiveJob in test environment
    Fixing specs on Travis CI

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -5,7 +5,7 @@
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  
+
   config.cache_classes = false
 
   # Do not eager load code on boot. This avoids loading your whole application
@@ -36,6 +36,8 @@ Rails.application.configure do
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
+
+  config.active_job.queue_adapter = :test
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
