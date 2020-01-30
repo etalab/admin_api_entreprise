feefa91bcf5d2e553e3f36a3a21ec1e7c8840322
Alexandre De Pablo <alexandre.depablo@gmail.com>
2020-01-30 15:06:28 +0100

    Update Signup script import with last validated requests

--- app/concepts/access_demand/operation/import_from_ds.rb
+++ app/concepts/access_demand/operation/import_from_ds.rb
@@ -11,6 +11,7 @@ module AccessDemand::Operation
 
     def import_users_siret(ctx, csv_reader:, **)
       csv_reader.line_by_line do |line|
+        puts line['Email']
         next if ignore_request?(line)
 
         email_from_dump = line['Email'].downcase
@@ -35,6 +36,7 @@ module AccessDemand::Operation
       return true if line['ID'] == '377645' # Test de V.M.
       return true if line['ID'] == '394171' # J'ai retrouvé le jeton grâce au siret 24400067500151, le user a été supprimé...
       return true if line['ID'] == '466577' # Aucune entrée en base correspondante pour cette demande...
+      return true if line['ID'] == '336048' # User deleted as he now uses ATEXO
 
       false
     end
