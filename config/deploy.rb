040e2fdae41b2b3a02ba971f5dc1eca813c5cece
Alexandre De Pablo <alexandre.depablo@gmail.com>
2020-05-28 12:10:51 +0200

    Set symlinks with mina for environment keys

--- config/deploy.rb
+++ config/deploy.rb
@@ -57,6 +57,9 @@ set :shared_dirs, fetch(:shared_dirs, []).push(
   'tmp/cache'
 )
 set :shared_files, fetch(:shared_files, []).push(
+  'config/credentials/sandbox.key',
+  'config/credentials/staging.key',
+  'config/credentials/production.key',
   'config/database.yml',
   'config/sidekiq.yml',
   'config/master.key',
