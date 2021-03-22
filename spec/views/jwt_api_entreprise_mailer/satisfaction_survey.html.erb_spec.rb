f42d2ac5bc88e6c6e6b43f120b1d4b9306569dbf
Cyril Kato <cyril.kato@data.gouv.fr>
2021-03-22 17:19:32 +0100

    feat(mail): embed in to satisfaction_survey template a typeform option
    See https://help.typeform.com/hc/en-us/articles/360029664091-Launch-your-typeform-from-an-email

--- spec/views/jwt_api_entreprise_mailer/satisfaction_survey.html.erb_spec.rb
+++ spec/views/jwt_api_entreprise_mailer/satisfaction_survey.html.erb_spec.rb
@@ -3,18 +3,64 @@ require 'rails_helper'
 RSpec.describe 'rendering the mail template' do
   before do
     assign(:full_name, full_name)
-    render template: 'jwt_api_entreprise_mailer/satisfaction_survey.html.erb'
+    render template: "jwt_api_entreprise_mailer/satisfaction_survey.#{format}.erb"
   end
 
-  context 'with a name' do
-    let(:full_name) { 'Bob MARTIN' }
-
-    it { expect(rendered).to match /Bonjour Bob MARTIN,/ }
+  let(:embedded_form_truncated_content) do
+    <<~EMBEDDED.chomp
+      quelle est votre satisfaction</strong> concernant votre
+    EMBEDDED
   end
 
-  context 'without a name' do
-    let(:full_name) { nil }
+  let(:link_to_the_form) do
+    <<~LINK.chomp
+      <a href="https://startupdetat.typeform.com/to/bFo1H9NJ">Répondre à l&#39;enquête utilisateur</a>
+    LINK
+  end
 
-    it { expect(rendered).to match /Bonjour,/ }
+  subject do
+    rendered
+  end
+
+  context 'when in HTML' do
+    let(:format) { 'html' }
+
+    context 'with a name' do
+      let(:full_name) { 'Bob MARTIN' }
+
+      it { expect(subject).to match /Bonjour Bob MARTIN,/ }
+    end
+
+    context 'without a name' do
+      let(:full_name) { nil }
+
+      it { expect(subject).to match /Bonjour,/ }
+    end
+
+    after do
+      expect(subject).to match embedded_form_truncated_content
+      expect(subject).not_to match link_to_the_form
+    end
+  end
+
+  context 'when in plaintext' do
+    let(:format) { 'text' }
+
+    context 'with a name' do
+      let(:full_name) { 'Bob MARTIN' }
+
+      it { expect(subject).to match /Bonjour Bob MARTIN,/ }
+    end
+
+    context 'without a name' do
+      let(:full_name) { nil }
+
+      it { expect(subject).to match /Bonjour,/ }
+    end
+
+    after do
+      expect(subject).not_to match embedded_form_truncated_content
+      expect(subject).to match link_to_the_form
+    end
   end
 end
