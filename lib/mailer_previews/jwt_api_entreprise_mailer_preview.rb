9470d68715dc69a8d5cb17236fbaa86b77e380bc
Cyril Kato <cyril.kato@data.gouv.fr>
2021-03-29 14:54:13 +0200

    docs(mailer): remove comments about previewing emails
    Useful links:
    
     * https://guides.rubyonrails.org/action_mailer_basics.html\#previewing-emails
     * http://localhost:3000/rails/mailers/jwt_api_entreprise_mailer/satisfaction_survey

--- lib/mailer_previews/jwt_api_entreprise_mailer_preview.rb
+++ lib/mailer_previews/jwt_api_entreprise_mailer_preview.rb
@@ -1,6 +1,4 @@
-# @see https://guides.rubyonrails.org/action_mailer_basics.html#previewing-emails
 class JwtApiEntrepriseMailerPreview < ActionMailer::Preview
-  # @see http://localhost:3000/rails/mailers/jwt_api_entreprise_mailer/satisfaction_survey
   def satisfaction_survey
     token_owner_email = 'bob@gmail.com'
     token_authorization_request_id = 42
