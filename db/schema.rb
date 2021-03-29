41eaadd7f130a80a6c1f8c936e54900fe78e0827
Cyril Kato <cyril.kato@data.gouv.fr>
2021-03-29 14:20:52 +0200

    perf(db): add index on created_at to jwt_api_entreprises

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_26_095330) do
+ActiveRecord::Schema.define(version: 2021_03_29_121551) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 2021_03_26_095330) do
     t.boolean "archived", default: false
     t.string "temp_use_case"
     t.string "authorization_request_id"
+    t.index ["created_at"], name: "index_jwt_api_entreprises_on_created_at"
     t.index ["iat"], name: "index_jwt_api_entreprises_on_iat"
     t.index ["user_id"], name: "index_jwt_api_entreprises_on_user_id"
   end
