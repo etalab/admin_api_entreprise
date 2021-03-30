180f23ce4f2ecfdf4d3bcfca4348a83ae42ddc0d
Cyril Kato <cyril.kato@data.gouv.fr>
2021-03-30 20:33:27 +0200

    test(JwtApiEntreprise::Operation::AccessRequestSatisfactionSurvey): integration testing

--- spec/concepts/jwt_api_entreprise/operation/access_request_satisfaction_survey_spec.rb
+++ spec/concepts/jwt_api_entreprise/operation/access_request_satisfaction_survey_spec.rb
@@ -3,33 +3,53 @@ require 'rails_helper'
 RSpec.describe JwtApiEntreprise::Operation::AccessRequestSatisfactionSurvey do
   subject(:operation) { described_class }
 
-  before do
-    expect(JwtApiEntreprise).to receive(:satisfaction_survey_eligible_tokens).and_call_original
-  end
+  let!(:token) { create(:jwt_api_entreprise, datetime_of_issue, sent_state) }
 
-  let!(:token) { create(:jwt_api_entreprise, iat: iat_value) }
+  context 'when the waiting time is not reached' do
+    let(:datetime_of_issue) { :less_than_seven_days_ago }
 
-  let(:token_id) { token.id }
-  let(:token_owner_email) { token.user.email }
-  let(:token_authorization_request_id) { token.authorization_request_id }
+    context 'when the mail is not sent' do
+      let(:sent_state) { :access_request_survey_not_sent }
 
-  context 'when tokens are not eligible to survey' do
-    let(:iat_value) { attributes_for(:jwt_api_entreprise, :less_than_seven_days_ago).fetch(:iat) }
+      it 'does not send any email to token owners' do
+        expect {
+          operation.call
+        }.not_to have_enqueued_mail(JwtApiEntrepriseMailer, :satisfaction_survey)
+      end
+    end
 
-    it 'does not send any email to token owners' do
-      expect {
-        operation.call
-      }.not_to have_enqueued_mail(JwtApiEntrepriseMailer, :satisfaction_survey)
+    context 'when the mail is sent' do
+      let(:sent_state) { :access_request_survey_sent }
+
+      it 'does not send any email to token owners' do
+        expect {
+          operation.call
+        }.not_to have_enqueued_mail(JwtApiEntrepriseMailer, :satisfaction_survey)
+      end
     end
   end
 
-  context 'when tokens are eligible to survey' do
-    let(:iat_value) { attributes_for(:jwt_api_entreprise, :about_seven_days_ago).fetch(:iat) }
+  context 'when the waiting time is reached' do
+    let(:datetime_of_issue) { :about_seven_days_ago }
 
-    it 'sends an email to token owners' do
-      expect {
-        operation.call
-      }.to have_enqueued_mail(JwtApiEntrepriseMailer, :satisfaction_survey).with(args: [token_id, token_owner_email, token_authorization_request_id])
+    context 'when the mail is not sent' do
+      let(:sent_state) { :access_request_survey_not_sent }
+
+      it 'sends an email to token owners' do
+        expect {
+          operation.call
+        }.to have_enqueued_mail(JwtApiEntrepriseMailer, :satisfaction_survey).with(args: [token.id, token.user.email, token.authorization_request_id])
+      end
+    end
+
+    context 'when the mail is sent' do
+      let(:sent_state) { :access_request_survey_sent }
+
+      it 'does not send any email to token owners' do
+        expect {
+          operation.call
+        }.not_to have_enqueued_mail(JwtApiEntrepriseMailer, :satisfaction_survey)
+      end
     end
   end
 end
