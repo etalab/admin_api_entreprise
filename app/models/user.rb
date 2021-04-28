c53a4fca4a21a666951ec788b1196e06743919c2
Alexandre De Pablo <alexandre.depablo@gmail.com>
2021-04-28 20:10:43 +0200

    Move random token method into it's own concern for usage between models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  include RandomToken
+
   has_many :jwt_api_entreprise, dependent: :nullify
   has_many :contacts, through: :jwt_api_entreprise
 
@@ -15,16 +17,5 @@ class User < ApplicationRecord
 
   def generate_pwd_renewal_token
     update(pwd_renewal_token: random_token_for(:pwd_renewal_token))
-  end
-
-  private
-
-  def random_token_for(attr)
-    constraint = {}
-    loop do
-      token = SecureRandom.hex(10)
-      constraint[attr] = token
-      return token unless User.find_by(constraint)
-    end
   end
 end
