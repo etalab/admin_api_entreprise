e832c61722eca7df4a2da801b5e07a72e10f60c0
Alexandre De Pablo <alexandre.depablo@gmail.com>
2021-05-03 16:39:21 +0200

    Add mailer to send magic links

--- app/mailers/jwt_api_entreprise_mailer.rb
+++ app/mailers/jwt_api_entreprise_mailer.rb
@@ -35,6 +35,10 @@ class JwtApiEntrepriseMailer < ApplicationMailer
   end
 
   def magic_link(recipient, jwt)
+    @jwt = jwt
+    @magic_link_url = "#{Rails.configuration.jwt_magic_link_url}?token=#{jwt.magic_link_token}"
+    subject = 'API Entreprise - Lien d\'accès à votre jeton !'
 
+    mail(to: recipient, subject: subject)
   end
 end
