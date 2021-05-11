607dfdeb622f5ee7520ed1fea1f36e7860cf4580
Cyril Kato <cyril.kato@data.gouv.fr>
2021-05-11 16:15:34 +0200

    test(MailjetContacts::Operation::Create): expect instead of allow

--- spec/concepts/mailjet_contact/operation/create_spec.rb
+++ spec/concepts/mailjet_contact/operation/create_spec.rb
@@ -5,25 +5,6 @@ RSpec.describe MailjetContacts::Operation::Create do
 
   before do
     create(:contact, :tech, jwt_api_entreprise: jwt_api_entreprise)
-
-    allow(Mailjet::Contactslist_managemanycontacts).to receive(:create).with(
-      action: 'addnoforce',
-      contacts: [
-        {
-          'email': user.email,
-          'properties': {
-            'contact_demandeur':  true,
-            'contact_métier':     true,
-            'contact_technique':  true,
-            'infolettre':         true,
-            'origine':            'dashboard',
-            'techlettre':         true
-          }
-        }
-      ],
-      id: Rails.application.credentials.mj_list_id!
-    )
-
     create(:contact, :business, jwt_api_entreprise: jwt_api_entreprise)
     create(:contact, :other, jwt_api_entreprise: jwt_api_entreprise)
   end
@@ -35,12 +16,36 @@ RSpec.describe MailjetContacts::Operation::Create do
   let(:jwt_api_entreprise) { create(:jwt_api_entreprise, user: user) }
 
   context 'when users were added a long time ago' do
+    before do
+      expect(Mailjet::Contactslist_managemanycontacts).to_not receive(:create)
+    end
+
     let(:created_date) { 2.years.ago }
 
     it { is_expected.to be_a_failure }
   end
 
   context 'when users were recently added' do
+    before do
+      expect(Mailjet::Contactslist_managemanycontacts).to receive(:create).with(
+        action: 'addnoforce',
+        contacts: [
+          {
+            'email': user.email,
+            'properties': {
+              'contact_demandeur':  true,
+              'contact_métier':     true,
+              'contact_technique':  true,
+              'infolettre':         true,
+              'origine':            'dashboard',
+              'techlettre':         true
+            }
+          }
+        ],
+        id: Rails.application.credentials.mj_list_id!
+      )
+    end
+
     let(:created_date) { 2.minutes.ago }
 
     it { is_expected.to be_a_success }
