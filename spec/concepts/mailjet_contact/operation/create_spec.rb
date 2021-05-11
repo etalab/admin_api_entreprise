07c2319d329e0a0e78704f5d7c6ea6c54e08253f
Cyril Kato <cyril.kato@data.gouv.fr>
2021-05-11 15:23:58 +0200

    test(MailjetContacts::Operation::Create): specify the sent data structure

--- spec/concepts/mailjet_contact/operation/create_spec.rb
+++ spec/concepts/mailjet_contact/operation/create_spec.rb
@@ -4,9 +4,35 @@ RSpec.describe MailjetContacts::Operation::Create do
   subject { described_class.call }
 
   before do
-    allow(Mailjet::Contactslist_managemanycontacts).to receive(:create)
+    create(:contact, :tech, jwt_api_entreprise: jwt_api_entreprise)
+
+    allow(Mailjet::Contactslist_managemanycontacts).to receive(:create).with(
+      action: 'addnoforce',
+      contacts: [
+        {
+          'email': user.email,
+          'properties': {
+            'contact_demandeur':  true,
+            'contact_métier':     true,
+            'contact_technique':  true,
+            'infolettre':         true,
+            'origine':            'dashboard',
+            'techlettre':         true
+          }
+        }
+      ],
+      id: Rails.application.credentials.mj_list_id!
+    )
+
+    create(:contact, :business, jwt_api_entreprise: jwt_api_entreprise)
+    create(:contact, :other, jwt_api_entreprise: jwt_api_entreprise)
+  end
+
+  let(:user) do
     create(:user, created_at: created_date)
   end
+
+  let(:jwt_api_entreprise) { create(:jwt_api_entreprise, user: user) }
 
   context 'when users were added a long time ago' do
     let(:created_date) { 2.years.ago }
