68c03ca4290b61ca5eddcb260053ef38341b0db1
Cyril Kato <cyril.kato@data.gouv.fr>
2021-05-12 15:26:28 +0200

    test(MailjetContacts::Operation::Create): remove some duplicated cases

--- spec/concepts/mailjet_contact/operation/create_spec.rb
+++ spec/concepts/mailjet_contact/operation/create_spec.rb
@@ -7,49 +7,23 @@ RSpec.describe MailjetContacts::Operation::Create do
     user.update_attribute(:created_at, creation_period)
   end
 
-  let(:user) { create(:contact, type).jwt_api_entreprise.user }
+  context 'when a user with a tech contact is present in the DB' do
+    let(:user) { create(:contact, :tech).jwt_api_entreprise.user }
 
-  context 'when users were added a long time ago' do
-    let(:creation_period) { Faker::Time.between(from: 10.years.ago, to: 1.day.ago) }
+    context 'when he was added a long time ago' do
+      let(:creation_period) { Faker::Time.between(from: 10.years.ago, to: 1.day.ago) }
 
-    context 'when the contact is tech' do
-      let(:type) { :tech }
-
-      it 'do not send any contacts to Mailjet' do
+      it 'is not posted to Mailjet' do
         expect(Mailjet::Contactslist_managemanycontacts).to_not receive(:create)
 
         is_expected.to be_a_failure
       end
     end
 
-    context 'when the contact is business' do
-      let(:type) { :business }
+    context 'when he was recently added' do
+      let(:creation_period) { Faker::Time.between(from: 1.day.ago + 1, to: Time.current) }
 
-      it 'do not send any contacts to Mailjet' do
-        expect(Mailjet::Contactslist_managemanycontacts).to_not receive(:create)
-
-        is_expected.to be_a_failure
-      end
-    end
-
-    context 'when the contact is other' do
-      let(:type) { :other }
-
-      it 'do not send any contacts to Mailjet' do
-        expect(Mailjet::Contactslist_managemanycontacts).to_not receive(:create)
-
-        is_expected.to be_a_failure
-      end
-    end
-  end
-
-  context 'when users were recently added' do
-    let(:creation_period) { Faker::Time.between(from: 1.day.ago + 1, to: Time.current) }
-
-    context 'when the contact is tech' do
-      let(:type) { :tech }
-
-      it 'does send the contact to Mailjet' do
+      it 'is serialized and posted to Mailjet' do
         expect(Mailjet::Contactslist_managemanycontacts).to receive(:create).with(
           action: 'addnoforce',
           contacts: [
@@ -62,58 +36,6 @@ RSpec.describe MailjetContacts::Operation::Create do
                 'infolettre':         true,
                 'origine':            'dashboard',
                 'techlettre':         true
-              }
-            }
-          ],
-          id: Rails.application.credentials.mj_list_id!
-        )
-
-        is_expected.to be_a_success
-      end
-    end
-
-    context 'when the contact is business' do
-      let(:type) { :business }
-
-      it 'does send the contact to Mailjet' do
-        expect(Mailjet::Contactslist_managemanycontacts).to receive(:create).with(
-          action: 'addnoforce',
-          contacts: [
-            {
-              'email': user.email,
-              'properties': {
-                'contact_demandeur':  false,
-                'contact_métier':     true,
-                'contact_technique':  false,
-                'infolettre':         true,
-                'origine':            'dashboard',
-                'techlettre':         false
-              }
-            }
-          ],
-          id: Rails.application.credentials.mj_list_id!
-        )
-
-        is_expected.to be_a_success
-      end
-    end
-
-    context 'when the contact is other' do
-      let(:type) { :other }
-
-      it 'does send the contact to Mailjet' do
-        expect(Mailjet::Contactslist_managemanycontacts).to receive(:create).with(
-          action: 'addnoforce',
-          contacts: [
-            {
-              'email': user.email,
-              'properties': {
-                'contact_demandeur':  true,
-                'contact_métier':     false,
-                'contact_technique':  false,
-                'infolettre':         true,
-                'origine':            'dashboard',
-                'techlettre':         false
               }
             }
           ],
