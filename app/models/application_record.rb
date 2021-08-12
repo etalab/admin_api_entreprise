19c2fe3ce77d785d15c0a2aa04e188ffb799b6a6
Loïc Delmaire <loic.delmaire@gmail.com>
2021-08-12 10:54:54 +0200

    Harden ApplicationRecord default_scope
    `created_at ASC` as string does not work with join tables, it detects it
    as ambigous.

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,7 +1,7 @@
 class ApplicationRecord < ActiveRecord::Base
   # Now UUID are used as models ID we cannot use it to assume the creation
   # order as the Rails default behaviour, so we use the creation timestamp
-  default_scope -> { order('created_at ASC') }
+  default_scope -> { order(created_at: :asc) }
 
   self.abstract_class = true
 end
