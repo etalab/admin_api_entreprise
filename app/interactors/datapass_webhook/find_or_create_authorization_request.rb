b27261b9525f4dc0139fe54e8bccde042104b875
Loïc Delmaire <loic.delmaire@gmail.com>
2021-08-17 17:11:55 +0200

    Introduce DatapassWebhook::FindOrCreateAuthorizationRequest

---
+++ app/interactors/datapass_webhook/find_or_create_authorization_request.rb
@@ -0,0 +1,72 @@
+class DatapassWebhook::FindOrCreateAuthorizationRequest < ApplicationInteractor
+  def call
+    context.authorization_request = AuthorizationRequest.find_or_initialize_by(external_id: context.data['pass']['id'])
+    context.authorization_request.assign_attributes(authorization_request_attributes)
+
+    context.authorization_request.user = context.user
+    create_or_update_contacts
+
+    return if context.authorization_request.save
+
+    context.fail!(message: 'Authorization request not valid')
+  end
+
+  private
+
+  def create_or_update_contacts
+    create_or_update_contact(:technique)
+    create_or_update_contact(:metier)
+  end
+
+  def create_or_update_contact(kind)
+    contact = context.authorization_request.public_send("contact_#{kind}") || context.authorization_request.public_send("build_contact_#{kind}")
+    contact_payload = contact_payload_for(kind)
+
+    contact.assign_attributes(
+      last_name: contact_payload['family_name'],
+      first_name: contact_payload['given_name'],
+      email: contact_payload['email'],
+      phone_number: contact_payload['phone_number'],
+    )
+
+    contact.save
+  end
+
+  def authorization_request_attributes
+    context.data['pass'].slice(
+      'intitule',
+      'description',
+    ).merge(authorization_request_attributes_for_current_event).merge(
+      'last_update' => fired_at_as_datetime,
+    )
+  end
+
+  def authorization_request_attributes_for_current_event
+    case context.event
+    when 'send_application'
+      if context.authorization_request.first_submitted_at.nil?
+        {
+          'first_submitted_at' => fired_at_as_datetime,
+        }
+      else
+        {}
+      end
+    when 'validate_application'
+      {
+        'validated_at' => fired_at_as_datetime,
+      }
+    else
+      {}
+    end
+  end
+
+  def contact_payload_for(kind)
+    context.data['pass']['contacts'].find do |contact_payload|
+      contact_payload['id'] == kind.to_s
+    end
+  end
+
+  def fired_at_as_datetime
+    @fired_at_as_datetime ||= Time.at(context.fired_at).to_datetime
+  end
+end
---
+++ spec/interactors/datapass_webhook/find_or_create_authorization_request_spec.rb
@@ -0,0 +1,121 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe DatapassWebhook::FindOrCreateAuthorizationRequest, type: :interactor do
+  subject { described_class.call(datapass_webhook_params.merge(user: user)) }
+
+  let(:datapass_webhook_params) { build(:datapass_webhook, fired_at: fired_at, authorization_request_attributes: { id: authorization_id }) }
+  let(:user) { create(:user) }
+  let(:authorization_id) { rand(1..9001) }
+  let(:fired_at) { 2.minutes.ago.to_i }
+
+  context 'when authorization request already exists' do
+    let!(:authorization_request) { create(:authorization_request, :with_contacts, external_id: authorization_id) }
+
+    it { is_expected.to be_a_success }
+    it { expect(subject.authorization_request).to eq(authorization_request) }
+
+    it 'updates attributes on existing authorization request' do
+      expect {
+        subject
+      }.to change { authorization_request.reload.intitule }.to(datapass_webhook_params['data']['pass']['intitule'])
+
+      expect(authorization_request.last_update.to_i).to eq(fired_at)
+    end
+
+    it 'updates contacts associated to authorization request' do
+      expect(authorization_request.contact_metier.full_name).not_to eq("metier last name metier first name")
+      expect(authorization_request.contact_technique.full_name).not_to eq("technique last name technique first name")
+
+      expect(authorization_request.contact_technique.email).not_to match(/technique\d+@/)
+      expect(authorization_request.contact_metier.email).not_to match(/metier\d+@/)
+
+      expect {
+        subject
+      }.not_to change { authorization_request.reload.contacts.count }
+
+      expect(authorization_request.contact_technique.email).to match(/technique\d+@/)
+      expect(authorization_request.contact_metier.email).to match(/metier\d+@/)
+
+      expect(authorization_request.contact_metier.full_name).to eq("metier last name metier first name")
+      expect(authorization_request.contact_technique.full_name).to eq("technique last name technique first name")
+    end
+
+    context 'when it is the same user' do
+      let!(:authorization_request) { create(:authorization_request, external_id: authorization_id, user: user) }
+
+      it 'does not update user' do
+        expect {
+          subject
+        }.not_to change { authorization_request.reload.user }
+      end
+    end
+
+    context 'when it is not the same user' do
+      let!(:authorization_request) { create(:authorization_request, external_id: authorization_id) }
+
+      it 'updates user' do
+        expect {
+          subject
+        }.to change { authorization_request.reload.user }.to(user)
+      end
+    end
+  end
+
+  context 'when authorization request does not exist' do
+    it { is_expected.to be_a_success }
+    it { expect(subject.authorization_request).to an_instance_of(AuthorizationRequest) }
+
+    it 'creates a new authorization request with attributes, contacts and linked to user' do
+      expect {
+        subject
+      }.to change { user.reload.authorization_requests.count }.by(1)
+
+      authorization_request = user.authorization_requests.last
+
+      expect(authorization_request.intitule).to eq(datapass_webhook_params['data']['pass']['intitule'])
+      expect(authorization_request.contacts.count).to eq(2)
+      expect(authorization_request.contact_technique.email).to match(/technique\d+@/)
+      expect(authorization_request.contact_metier.email).to match(/metier\d+@/)
+    end
+  end
+
+  context 'when event is send_application' do
+    let(:datapass_webhook_params) { build(:datapass_webhook, event: 'send_application', fired_at: fired_at, authorization_request_attributes: { id: authorization_id }) }
+
+    let!(:authorization_request) { create(:authorization_request, :with_contacts, external_id: authorization_id, first_submitted_at: first_submitted_at) }
+
+    context 'when first_submitted_at is nil' do
+      let(:first_submitted_at) { nil }
+
+      it 'updates it' do
+        expect {
+          subject
+        }.to change { authorization_request.reload.first_submitted_at.to_i }.to(fired_at)
+      end
+    end
+
+    context 'when first_submitted_at is present' do
+      let(:first_submitted_at) { 3.days.ago }
+
+      it 'does not update it' do
+        expect {
+          subject
+        }.not_to change { authorization_request.reload.first_submitted_at.to_i }
+      end
+    end
+  end
+
+  context 'when event is validate_application' do
+    let(:datapass_webhook_params) { build(:datapass_webhook, event: 'validate_application', fired_at: fired_at, authorization_request_attributes: { id: authorization_id }) }
+
+    let!(:authorization_request) { create(:authorization_request, :with_contacts, external_id: authorization_id) }
+
+    it 'sets validated_at' do
+      expect {
+        subject
+      }.to change { authorization_request.reload.validated_at.to_i }.to(fired_at)
+    end
+  end
+end
