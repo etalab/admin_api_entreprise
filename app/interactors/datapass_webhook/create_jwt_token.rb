867b2fda556a3931c3e3d5b821ffb862ce55946e
Loïc Delmaire <loic.delmaire@gmail.com>
2021-08-17 17:25:48 +0200

    Introduce DatapassWebhook::CreateJwtToken

---
+++ app/interactors/datapass_webhook/create_jwt_token.rb
@@ -0,0 +1,45 @@
+class DatapassWebhook::CreateJwtToken < ApplicationInteractor
+  def call
+    return if context.event != 'validate_application'
+    return if token_already_exists?
+
+    token = create_jwt_token
+
+    if token.persisted?
+      affect_roles(token)
+      context.token_id = token.id
+    else
+      context.fail!(message: 'Fail to create token')
+    end
+  end
+
+  private
+
+  def create_jwt_token
+    authorization_request.create_jwt_api_entreprise(
+      subject: authorization_request.intitule,
+      authorization_request_id: authorization_request.external_id,
+      iat: Time.zone.now.to_i,
+      version: '1.0',
+      exp: 18.months.from_now.to_i
+    )
+  end
+
+  def affect_roles(token)
+    token.roles = Role.where(code: roles)
+  end
+
+  def token_already_exists?
+    context.authorization_request.jwt_api_entreprise.present?
+  end
+
+  def roles
+    context.data['pass']['scopes'].map do |code, bool|
+      code if bool
+    end.compact
+  end
+
+  def authorization_request
+    context.authorization_request
+  end
+end
---
+++ spec/interactors/datapass_webhook/create_jwt_token_spec.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe DatapassWebhook::CreateJwtToken, type: :interactor do
+  subject { described_class.call(datapass_webhook_params.merge(authorization_request: authorization_request)) }
+
+  let(:authorization_request) { create(:authorization_request) }
+  let(:datapass_webhook_params) { build(:datapass_webhook, event: event) }
+  let(:roles) { build(:datapass_webhook_pass_model)['scopes'].keys }
+
+  before do
+    Timecop.freeze
+
+    roles.each do |role|
+      create(:role, code: role)
+    end
+  end
+
+  after do
+    Timecop.return
+  end
+
+  context 'when event is not validate_application' do
+    let(:event) { 'whatever' }
+
+    it { is_expected.to be_a_success }
+
+    it 'does not create a new jwt token' do
+      expect {
+        subject
+      }.not_to change { JwtApiEntreprise.count }
+    end
+  end
+
+  context 'when event is validate_application' do
+    let(:event) { 'validate_application' }
+
+    it { is_expected.to be_a_success }
+
+    it { expect(subject.token_id).to be_present }
+
+    it 'creates a new jwt token with valid attributes and roles' do
+      expect {
+        subject
+      }.to change { JwtApiEntreprise.count }
+
+      token = JwtApiEntreprise.last
+
+      expect(token.authorization_request).to eq(authorization_request)
+      expect(token.exp).to eq(18.month.from_now.to_i)
+      expect(token.iat).to eq(Time.zone.now.to_i)
+
+      expect(token.roles.pluck(:code).sort).to eq(%w[associations entreprises])
+    end
+  end
+end
