66ed6c936a342cb19765c6e7e52d344b3008084d
Loïc Delmaire <loic.delmaire@gmail.com>
2021-08-18 15:05:03 +0200

    Implement DatapassWebhook organizer

---
+++ spec/lib/datapass_webhook_spec.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe DatapassWebhook, type: :interactor do
+  subject { described_class.call(datapass_webhook_params) }
+
+  let(:datapass_webhook_params) { build(:datapass_webhook, event: 'validate_application') }
+
+  before do
+    allow(Mailjet::Contactslist_managemanycontacts).to receive(:create)
+  end
+
+  it { is_expected.to be_a_success }
+
+  it 'creates a user' do
+    expect {
+      subject
+    }.to change { User.count }.by(1)
+  end
+
+  it 'creates an authorization request' do
+    expect {
+      subject
+    }.to change { AuthorizationRequest.count }.by(1)
+  end
+
+  it 'creates token and stores id in token_id' do
+    expect {
+      subject
+    }.to change { JwtApiEntreprise.count }.by(1)
+
+    token = JwtApiEntreprise.last
+
+    expect(subject.token_id).to eq(token.id)
+  end
+end
