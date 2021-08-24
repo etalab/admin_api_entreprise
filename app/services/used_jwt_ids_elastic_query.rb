13c786ba0eb6429ddce781904348a9d6c1b84d4e
H Lepetit <hugo.lepetit@protonmail.com>
2021-08-24 15:11:07 +0200

    Extracts used_jwt_ids querying logic in its own PORO

---
+++ app/services/used_jwt_ids_elastic_query.rb
@@ -0,0 +1,35 @@
+class UsedJwtIdsElasticQuery
+  def initialize(number_of_days_from_now: 30)
+    @number_of_days_from_now = number_of_days_from_now
+  end
+
+  def perform
+    client = ElasticClient.new
+    client.establish_connection
+
+    client.search(used_jti_ids_json_query, size: 0)
+
+    client.raw_response.dig('aggregations', 'unique-jti', 'buckets').map{ |bucket| bucket['key'] }
+  end
+
+  def used_jti_ids_json_query
+    {
+      "query" => {
+        "bool" => {
+          "must": [
+            { "match" =>  { "type" => "siade" }},
+            { "range" =>  { "@timestamp" => { "gte" => "now-730d/d", "lte" => "now/d" }}}
+          ]
+        }
+      },
+      "aggs" => {
+        "unique-jti" => {
+          "terms" => {
+            "field" => "user_access.jti",
+            "size"=> 50000
+          }
+        }
+      }
+    }
+  end
+end
---
+++ spec/services/used_jwt_ids_elastic_query_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+RSpec.describe UsedJwtIdsElasticQuery, type: :service do
+  let(:used_jwt_id_1){ "ab375719-986e-48b0-9bbb-d45fe9343cc3" }
+  let(:used_jwt_id_2){ "5af58470-f007-4102-8d48-016ce018de23" }
+  let(:used_jwt_id_3){ "fdc7196c-ba45-43b3-bf26-b0aaba44ee03" }
+
+  before do
+    allow_any_instance_of(ElasticClient).to receive(:establish_connection).and_return(true)
+
+    allow_any_instance_of(ElasticClient).to receive(:raw_response).and_return(true)
+    allow_any_instance_of(ElasticClient).to receive(:raw_response).and_return({
+      "took"=>2130,
+      "timed_out"=>false,
+      "_shards"=>{"total"=>29, "successful"=>29, "skipped"=>0, "failed"=>0},
+      "hits"=>{"total"=>{"value"=>10000, "relation"=>"gte"}, "max_score"=>nil, "hits"=>[]},
+      "aggregations"=> {
+        "unique-jti"=> {
+          "doc_count_error_upper_bound"=>0,
+          "sum_other_doc_count"=>0,
+          "buckets"=> [
+            {"key"=> used_jwt_id_1, "doc_count"=>8470146},
+            {"key"=> used_jwt_id_2, "doc_count"=>7800786},
+            {"key"=> used_jwt_id_3, "doc_count"=>7710650}
+          ]
+        }
+      }
+    })
+  end
+
+  subject { described_class.new }
+
+  it '#perform extracts used jwt ids and returns them' do
+    results = subject.perform
+
+    expect(results).to be_an(Array)
+    expect(results.size).to eq(3)
+    expect(results).to include(*[used_jwt_id_2, used_jwt_id_1, used_jwt_id_3])
+  end
+end
