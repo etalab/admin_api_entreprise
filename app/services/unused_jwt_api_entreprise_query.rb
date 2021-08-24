ea45dadd2fccd6c2e3242a5122aca27e27a784a5
H Lepetit <hugo.lepetit@protonmail.com>
2021-08-24 15:12:03 +0200

    Extract unused jwt_api_entreprise querying in its own PORO
    - Could have been a scope. Model already had scope related to its
    business responsibilities and data life cycle / state. This one is about
    analyzing what's there.
    
    - Could have been a scope but felt clunky with its parameter to put it
    out there, plus it depends on an external call to function

---
+++ app/services/unused_jwt_api_entreprise_query.rb
@@ -0,0 +1,13 @@
+class UnusedJwtApiEntrepriseQuery
+  def initialize(number_of_days_from_now: 30)
+    @number_of_days_from_now = number_of_days_from_now
+  end
+
+  def perform
+    JwtApiEntreprise.where.not(id: used_jwt_ids)
+  end
+
+  def used_jwt_ids
+    UsedJwtIdsElasticQuery.new(number_of_days_from_now: 730).perform
+  end
+end
---
+++ spec/services/unused_jwt_api_entreprise_query_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe UnusedJwtApiEntrepriseQuery, type: :service do
+  let(:used_jwt_id_1){ "ab375719-986e-48b0-9bbb-d45fe9343cc3" }
+  let(:used_jwt_id_2){ "5af58470-f007-4102-8d48-016ce018de23" }
+  let(:used_jwt_id_3){ "fdc7196c-ba45-43b3-bf26-b0aaba44ee03" }
+
+  let!(:used_jwt_1)   { create(:jwt_api_entreprise, id: used_jwt_id_1) }
+  let!(:used_jwt_2)   { create(:jwt_api_entreprise, id: used_jwt_id_2) }
+  let!(:used_jwt_3)   { create(:jwt_api_entreprise, id: used_jwt_id_3) }
+  let!(:unused_jwt_1) { create(:jwt_api_entreprise, id: 'not-used') }
+
+  before do
+    allow_any_instance_of(UsedJwtIdsElasticQuery).to receive(:perform).and_return(
+      [used_jwt_id_1, used_jwt_id_2, used_jwt_id_3]
+    )
+  end
+
+  it '#perform returns unused_jwts only' do
+    results = subject.perform
+
+    expect(results).not_to include(*[used_jwt_1, used_jwt_2, used_jwt_3])
+    expect(results).to include(unused_jwt_1)
+  end
+end
