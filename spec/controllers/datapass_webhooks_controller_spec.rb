4672afe41efe287452be92e66b6d2356f4b66f58
Loïc Delmaire <loic.delmaire@gmail.com>
2021-08-25 15:55:35 +0200

    Refactor tests on hub signature in the class object

--- spec/controllers/datapass_webhooks_controller_spec.rb
+++ spec/controllers/datapass_webhooks_controller_spec.rb
@@ -8,73 +8,26 @@ RSpec.describe DatapassWebhooksController, type: :controller do
 
     let(:params) do
       {
-        event: event,
-        model_type: 'Pass',
-        fired_at: Time.now.to_i,
-        data: {
-          pass: {
-            id: 9001
+        'event' => event,
+        'model_type' => 'Pass',
+        'fired_at' => Time.now.to_i.to_s,
+        'data' => {
+          'pass' => {
+            'id' => '9001'
           },
         }
       }
     end
 
-    let(:headers) do
-      {
-        'Content-Type' => 'application/json',
-        'X-Hub-Signature-256' => hub_signature,
-      }
-    end
-
     let(:event) { 'refused' }
-    let(:verify_token) { Rails.application.credentials.webhook_verify_token! }
-
-    before do
-      headers.each do |key, value|
-        request.headers[key] = value
-      end
-    end
 
     context 'without a valid hub signature' do
-      let(:hub_signature) { "sha256=#{OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha256'), 'lol', params.to_json)}" }
-
-      it 'does not call DatapassWebhook' do
-        expect(DatapassWebhook).not_to receive(:call)
-
-        subject
-      end
-
-      it 'renders 401' do
-        subject
-
-        expect(response.code).to eq('401')
-      end
-    end
-
-    context 'without hub signature' do
-      let(:headers) do
-        {
-          'Content-Type' => 'application/json',
-        }
+      before do
+        allow_any_instance_of(HubSignature).to receive(:valid?).and_return(false)
       end
 
       it 'does not call DatapassWebhook' do
         expect(DatapassWebhook).not_to receive(:call)
-
-        subject
-      end
-
-      it 'tracks through Sentry the incoming payload' do
-        expect(Sentry).to receive(:set_context).with(
-          'DataPass webhook incoming payload',
-          payload: params,
-        )
-        expect(Sentry).to receive(:capture_message).with(
-          'DataPass Incoming Payload',
-          {
-            level: 'info',
-          }
-        )
 
         subject
       end
@@ -87,11 +40,11 @@ RSpec.describe DatapassWebhooksController, type: :controller do
     end
 
     context 'with a valid hub signature' do
-      let(:hub_signature) { "sha256=#{OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha256'), verify_token, params.to_json)}" }
-
       let(:token_id) { 'token id' }
 
       before do
+        allow_any_instance_of(HubSignature).to receive(:valid?).and_return(true)
+
         allow(DatapassWebhook).to receive(:call).and_return(
           OpenStruct.new(
             token_id: token_id,
