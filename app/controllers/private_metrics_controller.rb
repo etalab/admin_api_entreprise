a49264804a1f51ed64a6197c9a0d3fb4dd6be6e8
H Lepetit <hugo.lepetit@protonmail.com>
2021-08-26 11:37:56 +0200

    Lets see some numbers in prod

--- app/controllers/private_metrics_controller.rb
+++ app/controllers/private_metrics_controller.rb
@@ -6,14 +6,39 @@ class PrivateMetricsController < ApplicationController
 
     render json: {
       unused_jwt_list: UnusedJwtApiEntrepriseQuery.new.perform,
-      #active_jwt_count: JwtApiEntreprise.where(id: used_jti).count,
-      #new_users_histogram: User.all.group_by{ |u| u.created_at.beginning_of_month }.map{ |d, results| [d.strftime('%Y-%m'), results.count] },
-      #user_wiht_fresh_token_not_used_even_though_it_was_before_last_week: JwtApiEntreprise.where.not(id: used_jti).where('created_at > ?', 1.week.ago.beginning_of_week).users.uniq,
-      #jwt_expiring_in_less_than_a_week:  JwtApiEntreprise.where('exp < ? AND exp > ?', Time.now.to_i + 1.week, Time.now.to_i),
-      #jwt_expiring_in_less_than_three_months_but_after_month:  JwtApiEntreprise.where('exp < ? AND exp > ?', Time.now.to_i + 3.month, Time.now.to_i + 1.month),
-      #jwt_expiring_in_less_than_a_month_but_after_1_week:  JwtApiEntreprise.where('exp < ? AND exp > ?', Time.now.to_i + 1.month, Time.now.to_i + 1.week)
-
+      jwt_expiring_in_less_than_1_week: jwt_expiring_in_less_than_1_week,
+      jwt_expiring_in_less_than_1_month_but_after_1_week: jwt_expiring_in_less_than_1_month_but_after_1_week,
+      jwt_expiring_in_less_than_3_months_but_after_1_month: jwt_expiring_in_less_than_3_months_but_after_1_month
     }, status: 200
   end
+
+  def jwt_expiring_in_less_than_1_week
+    JwtApiEntrepriseExpiringWithinIntervalQuery.new(
+      interval_start: now,
+      interval_stop: 1.week.from_now
+    ).perform.count
+  end
+
+  def jwt_expiring_in_less_than_1_month_but_after_1_week
+    JwtApiEntrepriseExpiringWithinIntervalQuery.new(
+      interval_start: 1.week.from_now.tomorrow,
+      interval_stop: now + 1.month
+    ).perform.count
+  end
+
+  def jwt_expiring_in_less_than_3_months_but_after_1_month
+    JwtApiEntrepriseExpiringWithinIntervalQuery.new(
+      interval_start: 1.month.from_now.tomorrow,
+      interval_stop: now + 3.month
+    ).perform.count
+  end
+
+  def now
+    Time.now
+  end
+
+  #new_users_histogram: User.all.group_by{ |u| u.created_at.beginning_of_month }.map{ |d, results| [d.strftime('%Y-%m'), results.count] },
+  #active_jwt_count:
+  #user_wiht_recent_unused_jwt_api_entreprise: JwtApiEntreprise.where.not(id: used_jti).where('created_at > ?', 1.week.ago.beginning_of_week).users.uniq,
 end
 
