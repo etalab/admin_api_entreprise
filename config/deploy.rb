e758e9cd387156df8118d70751411815a30fc907
Loïc Delmaire <loic.delmaire@gmail.com>
2021-09-08 18:28:02 +0200

    Update mina deploy config for assets compiling

--- config/deploy.rb
+++ config/deploy.rb
@@ -96,6 +96,7 @@ task :deploy => :environment do
     invoke :'deploy:link_shared_paths'
     invoke :'bundle:install'
     invoke :'rails:db_migrate'
+    invoke :'rails:assets_precompile'
     invoke :'deploy:cleanup'
 
     on :launch do
