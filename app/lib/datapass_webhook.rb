3cf2839cfb11d91d52c31aee25a0fa9fe14b6ee1
Loïc Delmaire <loic.delmaire@gmail.com>
2021-09-14 11:49:10 +0200

    Add ArchivePreviousToken to DatapassWebhook organizer

--- app/lib/datapass_webhook.rb
+++ app/lib/datapass_webhook.rb
@@ -4,6 +4,7 @@ class DatapassWebhook
   organize ::DatapassWebhook::FindOrCreateUser,
            ::DatapassWebhook::FindOrCreateAuthorizationRequest,
            ::DatapassWebhook::CreateJwtToken,
+           ::DatapassWebhook::ArchivePreviousToken,
            ::DatapassWebhook::UpdateMailjetContacts,
            ::DatapassWebhook::ExtractMailjetVariables,
            ::DatapassWebhook::ScheduleAuthorizationRequestEmails
--- spec/lib/datapass_webhook_spec.rb
+++ spec/lib/datapass_webhook_spec.rb
@@ -5,10 +5,14 @@ require 'rails_helper'
 RSpec.describe DatapassWebhook, type: :interactor do
   subject { described_class.call(datapass_webhook_params) }
 
-  let(:datapass_webhook_params) { build(:datapass_webhook, event: 'validate_application') }
+  let(:datapass_webhook_params) { build(:datapass_webhook, event: 'validate_application', authorization_request_attributes: { copied_from_enrollment_id: previous_enrollment_id }) }
+  let(:previous_enrollment_id) { rand(9001).to_s }
+  let(:jwt_api_entreprise) { create(:jwt_api_entreprise) }
 
   before do
     allow(Mailjet::Contactslist_managemanycontacts).to receive(:create)
+
+    create(:authorization_request, external_id: previous_enrollment_id, jwt_api_entreprise: jwt_api_entreprise)
   end
 
   it { is_expected.to be_a_success }
@@ -33,5 +37,11 @@ RSpec.describe DatapassWebhook, type: :interactor do
     token = JwtApiEntreprise.last
 
     expect(subject.token_id).to eq(token.id)
+  end
+
+  it 'archives previous token' do
+    expect {
+      subject
+    }.to change { jwt_api_entreprise.reload.archived }.to(true)
   end
 end
