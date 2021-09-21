e3a08d6c669015a137b443ef1fb0cc408556ab8d
Loïc Delmaire <loic.delmaire@gmail.com>
2021-09-21 12:16:51 +0200

    Scout commit

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -9,11 +9,8 @@ ActiveSupport::Inflector.inflections(:en) do |inflect|
 #   inflect.irregular 'person', 'people'
 #   inflect.uncountable %w( fish sheep )
 
-    inflect.irregular 'JwtApiEntreprise', 'JwtApiEntreprise'
-end
+  inflect.irregular 'JwtApiEntreprise', 'JwtApiEntreprise'
 
-# These inflection rules are supported but not enabled by default:
-ActiveSupport::Inflector.inflections(:en) do |inflect|
   inflect.acronym 'DB'
   inflect.acronym 'OAuth'
 end
