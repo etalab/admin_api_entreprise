da5474d64a457fa3649ccf8a25efb4c198a6e3f3
Loïc Delmaire <loic.delmaire@gmail.com>
2021-09-22 15:17:42 +0200

    Reload js assets on every page change (turbolinks)

--- app/assets/javascripts/dsfr.nomodule.js
+++ app/assets/javascripts/dsfr.nomodule.js
@@ -1,5 +1,7 @@
 /*! DSFR v1.1.0 | SPDX-License-Identifier: MIT | License-Filename: LICENCE.md | restricted use (see terms and conditions) */
 
+document.addEventListener('turbolinks:load', function() {
+
 (function () {
   'use strict';
 
@@ -1923,3 +1925,5 @@
 
 }());
 //# sourceMappingURL=dsfr.nomodule.js.map
+
+});
