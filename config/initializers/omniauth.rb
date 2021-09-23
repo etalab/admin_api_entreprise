f3476fcf8a0807f600c7375534d3254f5fd79d6a
Loïc Delmaire <loic.delmaire@gmail.com>
2021-09-23 13:52:50 +0200

    Require oauth strategy

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,5 @@
+require 'omni_auth/strategies/api_gouv'
+
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :api_gouv, Rails.configuration.oauth_api_gouv_client_id, Rails.application.credentials.oauth_api_gouv_client_secret
 end
