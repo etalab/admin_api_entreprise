0c2846db396c8a52306689c7dab3cc453619beb7
Alexandre De Pablo <alexandre.depablo@gmail.com>
2021-09-29 14:53:24 +0200

    Introduce logic displaying a user token

--- app/controllers/jwt_api_entreprise_controller.rb
+++ app/controllers/jwt_api_entreprise_controller.rb
@@ -2,7 +2,10 @@ class JwtAPIEntrepriseController < AuthenticatedUsersController
   before_action { authorize_user_resource_access!(user_resource_id) }
 
   def index
-
+    @tokens = User.find(current_user.id).jwt_api_entreprise
+      .unexpired
+      .not_blacklisted
+      .where(archived: false)
   end
 
   private
--- spec/features/jwt_api_entreprise_index_spec.rb
+++ spec/features/jwt_api_entreprise_index_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'User JWT listing', type: :feature do
-  let(:user) { create(:user) }
+  let(:user) { create(:user, :with_jwt) }
   subject(:jwt_index) { visit user_jwt_api_entreprise_index_path(user.id) }
 
   context 'when the user is not authenticated' do
@@ -32,6 +32,44 @@ RSpec.describe 'User JWT listing', type: :feature do
         jwt_index
 
         expect(page.current_path).to eq(user_jwt_api_entreprise_index_path(user.id))
+      end
+
+      it 'lists the user\'s active tokens' do
+        jwt_index
+
+        user.jwt_api_entreprise.each do |jwt|
+          expect(page).to have_content(jwt.rehash)
+        end
+      end
+
+      it 'does not list other users tokens' do
+        another_user = create(:user, :with_jwt)
+        jwt_index
+
+        another_user.jwt_api_entreprise.each do |jwt|
+          expect(page).to_not have_content(jwt.rehash)
+        end
+      end
+
+      it 'does not display archived tokens' do
+        archived_jwt = create(:jwt_api_entreprise, :archived, user: user)
+        jwt_index
+
+        expect(page).to_not have_content(archived_jwt.rehash)
+      end
+
+      it 'does not display blacklisted tokens' do
+        blacklisted_jwt = create(:jwt_api_entreprise, :blacklisted, user: user)
+        jwt_index
+
+        expect(page).to_not have_content(blacklisted_jwt.rehash)
+      end
+
+      it 'does not display expired tokens' do
+        expired_jwt = create(:jwt_api_entreprise, :expired)
+        jwt_index
+
+        expect(page).to_not have_content(expired_jwt.rehash)
       end
     end
   end
