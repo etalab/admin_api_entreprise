3b99f4adb622962879d8bad5d01454c14d2c7dfb
Alexandre De Pablo <alexandre.depablo@gmail.com>
2021-10-05 19:17:27 +0200

    Enhance feature specs for account transfer

--- spec/features/transfer_account_spec.rb
+++ spec/features/transfer_account_spec.rb
@@ -15,40 +15,53 @@ RSpec.describe 'transfer user account ownership', type: :feature do
 
   before { log_as(user) }
 
-  it 'calls the underlying operation' do
-    expect(User::Operation::TransferOwnership)
-      .to receive(:call)
-      .with(params: { id: user.id, email: email })
-      .and_call_original
+  shared_examples :it_aborts_the_transfer do
+    it 'displays an error' do
+      subject
 
-    subject
+      expect(page).to have_css('.alert')
+    end
+
+    it 'does not transfer any tokens' do
+      user_tokens_id = user.jwt_api_entreprise.pluck(:id)
+      subject
+
+      expect(user.jwt_api_entreprise.reload.pluck(:id))
+        .to contain_exactly(*user_tokens_id)
+    end
   end
 
   context 'when no email address is provided' do
     let(:email) { '' }
 
-    it 'returns an error' do
-      subject
-
-      expect(page).to have_css('.alert')
-    end
+    it_behaves_like :it_aborts_the_transfer
   end
 
   context 'when the provided email address is invalid' do
     let(:email) { 'not a valid email' }
 
-    it 'returns an error' do
-      subject
-
-      expect(page).to have_css('.alert')
-    end
+    it_behaves_like :it_aborts_the_transfer
   end
 
   context 'when the provided email address is valid' do
-    it 'works' do
+    it 'displays success' do
       subject
 
       expect(page).to have_css('.notice')
+    end
+
+    it 'deletes the tokens from the previous account' do
+      subject
+
+      expect(user.jwt_api_entreprise).to be_empty
+    end
+
+    it 'transfer the tokens to the target account' do
+      tokens_id = user.jwt_api_entreprise.pluck(:id)
+      subject
+      target_user = User.find_by(email: email)
+
+      expect(target_user.jwt_api_entreprise.pluck(:id)).to include(*tokens_id)
     end
   end
 end
