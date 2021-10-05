938826896d2cad2c1ce98444982ffe83db6f9578
Loïc Delmaire <loic.delmaire@gmail.com>
2021-10-05 12:09:04 +0200

    Clean JwtAPIEntrepriseMailer#expiration_notice

--- app/mailers/jwt_api_entreprise_mailer.rb
+++ app/mailers/jwt_api_entreprise_mailer.rb
@@ -1,13 +1,4 @@
 class JwtAPIEntrepriseMailer < ApplicationMailer
-  def expiration_notice(jwt, nb_days)
-    @jwt = jwt
-    @nb_days = nb_days
-    recipients = @jwt.user_and_contacts_email
-    subject = "API Entreprise - Votre jeton expire dans #{@nb_days} jours !"
-
-    mail(to: recipients, subject: subject)
-  end
-
   def satisfaction_survey(jwt)
     @jwt = jwt
     recipient = @jwt.user.email
