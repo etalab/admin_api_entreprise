abc0d46f8d718894a36d6e46ee55662f661381bf
Loïc Delmaire <loic.delmaire@gmail.com>
2021-10-05 12:05:06 +0200

    Change JwtAPIEntrepriseExpirationNoticeJob scheduling sequence

--- app/jobs/jwt_api_entreprise_expiration_notice_job.rb
+++ app/jobs/jwt_api_entreprise_expiration_notice_job.rb
@@ -7,9 +7,6 @@ class JwtAPIEntrepriseExpirationNoticeJob < ApplicationJob
     JwtAPIEntreprise::Operation::NotifyExpiration.call(expire_in: 30)
     JwtAPIEntreprise::Operation::NotifyExpiration.call(expire_in: 14)
     JwtAPIEntreprise::Operation::NotifyExpiration.call(expire_in: 7)
-    JwtAPIEntreprise::Operation::NotifyExpiration.call(expire_in: 5)
-    JwtAPIEntreprise::Operation::NotifyExpiration.call(expire_in: 3)
-    JwtAPIEntreprise::Operation::NotifyExpiration.call(expire_in: 2)
-    JwtAPIEntreprise::Operation::NotifyExpiration.call(expire_in: 1)
+    JwtAPIEntreprise::Operation::NotifyExpiration.call(expire_in: 0)
   end
 end
