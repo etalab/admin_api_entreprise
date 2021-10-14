2464fcbe06d13245492257c08ee97a98b54ce775
Alexandre De Pablo <alexandre.depablo@gmail.com>
2021-10-14 18:10:18 +0200

    Harden lookup for a token from a magic token

--- app/concepts/jwt_api_entreprise/operation/retrieve_from_magic_link.rb
+++ app/concepts/jwt_api_entreprise/operation/retrieve_from_magic_link.rb
@@ -1,10 +1,15 @@
 module JwtAPIEntreprise::Operation
   class RetrieveFromMagicLink < Trailblazer::Operation
+    step :magic_token?
     step :retrieve_from_magic_link_token
     step :magic_link_token_valid?
 
-    def retrieve_from_magic_link_token(ctx, params:, **)
-      ctx[:jwt] = JwtAPIEntreprise.find_by_magic_link_token(params[:token])
+    def magic_token?(ctx, params:, **)
+      ctx[:magic_token] = params[:token]
+    end
+
+    def retrieve_from_magic_link_token(ctx, magic_token:, **)
+      ctx[:jwt] = JwtAPIEntreprise.find_by(magic_link_token: magic_token)
     end
 
     def magic_link_token_valid?(ctx, jwt:, **)
