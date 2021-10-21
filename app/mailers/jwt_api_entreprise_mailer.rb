48ea324315cdb25b8dc94328155625b7a1c7af69
Loïc Delmaire <loic.delmaire@gmail.com>
2021-10-21 16:33:49 +0200

    Remove JwtAPIEntrepriseMailer.satisfaction_survey

--- app/mailers/jwt_api_entreprise_mailer.rb
+++ app/mailers/jwt_api_entreprise_mailer.rb
@@ -1,11 +1,4 @@
 class JwtAPIEntrepriseMailer < ApplicationMailer
-  def satisfaction_survey(jwt)
-    @jwt = jwt
-    recipient = @jwt.user.email
-
-    mail(to: recipient, subject: 'API Entreprise - Comment s\'est déroulée votre demande d\'accès ?')
-  end
-
   def magic_link(recipient, jwt)
     @jwt = jwt
     @magic_link_url = Rails.configuration.jwt_magic_link_url + jwt.magic_link_token
