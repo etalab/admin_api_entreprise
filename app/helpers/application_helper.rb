c817ba0bb9b71397177a684ccfe31d6436c710bf
Loïc Delmaire <loic.delmaire@gmail.com>
2021-10-28 14:47:15 +0200

    Can convert GFM Markdown to HTML
    Related https://github.github.com/gfm/

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,6 @@
+require 'kramdown'
+require 'kramdown-parser-gfm'
+
 module ApplicationHelper
   def status_to_color(status)
     case status
@@ -9,6 +12,16 @@ module ApplicationHelper
       'info'
     else
       'undefined'
+    end
+  end
+
+  def markdown_to_html(content)
+    content_tag(:div, class: %(markdown-wrapper)) do
+      Kramdown::Document.new(
+        content,
+        input: 'GFM',
+        parse_block_html: true
+      ).to_html.html_safe
     end
   end
 end
