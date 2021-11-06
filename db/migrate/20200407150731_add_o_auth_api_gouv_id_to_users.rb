72f2b72047f3ebcd441840a2ebf12c7160b845bc
Hugo Lepetit <giglemad@gmail.com>
2021-11-06 08:58:30 +0100

    Fixes old migration following api -> API inflexion change

--- db/migrate/20200407150731_add_o_auth_api_gouv_id_to_users.rb
+++ db/migrate/20200407150731_add_o_auth_api_gouv_id_to_users.rb
@@ -1,4 +1,4 @@
-class AddOAuthApiGouvIdToUsers < ActiveRecord::Migration[6.0]
+class AddOAuthAPIGouvIdToUsers < ActiveRecord::Migration[6.0]
   def change
     add_column :users, :oauth_api_gouv_id, :integer, default: nil
   end
--- db/migrate/20210324150631_change_users_oauth_api_gouv_id_to_string.rb
+++ db/migrate/20210324150631_change_users_oauth_api_gouv_id_to_string.rb
@@ -1,4 +1,4 @@
-class ChangeUsersOAuthApiGouvIdToString < ActiveRecord::Migration[6.1]
+class ChangeUsersOAuthAPIGouvIdToString < ActiveRecord::Migration[6.1]
   def change
     change_column :users, :oauth_api_gouv_id, :string
   end
