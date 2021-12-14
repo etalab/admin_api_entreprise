f0996ee6bcf6229709dc51324fe9b1aec50b7568
Alexandre De Pablo <alexandre.depablo@gmail.com>
2021-12-14 13:07:51 +0100

    Introduce interactor to retrieve a token from a magic link

---
+++ spec/interactors/token/retrieve_from_magic_link_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe Token::RetrieveFromMagicLink do
+  subject { described_class.call(params) }
+
+  let(:params) do
+    { magic_token: magic_token }
+  end
+
+  let!(:jwt) { create(:jwt_api_entreprise, :with_magic_link) }
+  let(:magic_token) { jwt.magic_link_token }
+
+  context 'when the magic token is not provided' do
+    before { params.delete(:magic_token) }
+
+    it { is_expected.to be_a_failure }
+  end
+
+  context 'when the magic token does not exist' do
+    let(:magic_token) { 'not a magic link token' }
+
+    it { is_expected.to be_a_failure }
+  end
+
+  context 'when the magic token exists but is expired' do
+    it 'is a failure' do
+      Timecop.freeze(Time.zone.now + 4.hours) do
+        expect(subject).to be_a_failure
+      end
+    end
+  end
+
+  context 'when the magic token is valid' do
+    it { is_expected.to be_a_success }
+
+    its([:jwt]) { is_expected.to eq(jwt) }
+  end
+end
