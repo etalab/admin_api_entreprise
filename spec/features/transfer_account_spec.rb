3da7835edf0d2f6eb4c1419aa2383fd2a52773c3
Loïc Delmaire <loic.delmaire@gmail.com>
2021-12-30 11:05:25 +0100

    Refactor modal management, use turbo
    Migrate only one modal for now: user transfer account

--- spec/features/transfer_account_spec.rb
+++ spec/features/transfer_account_spec.rb
@@ -1,15 +1,21 @@
 require 'rails_helper'
 
-RSpec.describe 'transfer user account ownership', type: :feature do
+RSpec.describe 'transfer user account ownership', type: :feature, js: true do
   let(:user) { create(:user, :with_jwt) }
   let(:form_dom_id) { '#transfer_account' }
   let(:email) { 'valid@email.com' }
 
   subject do
     visit user_profile_path
+
+    click_on 'transfer_account_button'
+
     within form_dom_id do
       fill_in 'email', with: email
-      click_button
+
+      accept_prompt do
+        click_button
+      end
     end
   end
 
@@ -33,31 +39,15 @@ RSpec.describe 'transfer user account ownership', type: :feature do
     it_behaves_like :it_aborts_the_user_account_transfer
   end
 
-  context 'when the provided email address is invalid' do
-    let(:email) { 'not a valid email' }
-
-    it_behaves_like :it_aborts_the_user_account_transfer
-  end
-
   context 'when the provided email address is valid' do
     it_behaves_like :it_succeeds_the_user_account_transfer
   end
 
-  describe 'with javascript actived', js: true do
-    it 'works' do
-      visit user_profile_path
-      expect(page).not_to have_css(form_dom_id)
-      click_on("transfer_account_button")
-
-      expect(page).to have_css(form_dom_id)
-    end
-  end
-
-  describe 'transfering another user account' do
+  describe 'transfering another user account', js: false do
     let(:another_user) { create(:user) }
 
     subject do
-      page.driver.post(user_transfer_account_path(another_user), params: {
+      page.driver.post(user_transfer_account_index_path(user_id: another_user.id), params: {
         email: 'hackerman@email.fr'
       })
     end
