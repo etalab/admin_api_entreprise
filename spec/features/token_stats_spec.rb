32dc5e230e34e2c3d4e4d0c49df4e3ee8dfb2ab2
Loïc Delmaire <loic.delmaire@gmail.com>
2021-12-30 16:31:41 +0100

    Harden token pages states specs
    Specs were false: by default it is the 8 days period, that's why click
    and default tests before didn't pass with the new DSFR (not sure why it
    worked before...)

--- spec/features/token_stats_spec.rb
+++ spec/features/token_stats_spec.rb
@@ -41,118 +41,96 @@ RSpec.describe 'stats page for a token', type: :feature do
     end
 
     describe 'the ratio of success and errors per providers', js: true do
-      shared_examples :show_the_last_10_minutes_stats do
-        it 'displays the corresponding period (no stats for 10mn in fixtures file)' do
-          subject
-
-          expect(page).to have_table('calls_ratio_last_10_minutes', with_rows: [])
-        end
-
-        it 'does not display stats from other periods' do
-          subject
-
-          expect(page).not_to have_table('calls_ratio_last_8_hours')
-        end
-      end
-
       context 'when visiting the page the first time' do
         subject { visit token_stats_path(token) }
 
-        it_behaves_like :show_the_last_10_minutes_stats
-      end
-
-      context 'when clicking on a tab' do
-        subject do
-          visit token_stats_path(token)
-          click_on("tabpanel-#{period}")
+        let(:period) { 'last_8_days' }
+        let(:expected_stats) do
+          stats = JSON.parse(body, symbolize_names: true)
+          stats[:apis_usage][period.to_sym][:by_endpoint].first
         end
 
-        context 'for the last 10 minutes period' do
-          let(:period) { 'last_10_minutes' }
+        it 'displays the 8 days period' do
+          subject
 
-          it_behaves_like :show_the_last_10_minutes_stats
+          expect(page).to have_table("calls_ratio_#{period}", with_rows: [])
         end
 
-        context 'for the last 30 hours period' do
-          let(:period) { 'last_30_hours' }
+        it 'does not display stats from periods which are not 8 days' do
+          subject
 
-          it 'displays the corresponding period (no stats for 30h in fixtures file)' do
-            subject
-
-            expect(page).to have_table("calls_ratio_#{period}", with_rows: [])
-          end
-
-          it 'does not display stats from other periods' do
-            subject
-
-            expect(page).not_to have_table('calls_ratio_last_10_minutes')
-          end
+          expect(page).not_to have_table('calls_ratio_last_10_minutes')
         end
 
-        context 'for the last 8 days period' do
-          let(:period) { 'last_8_days' }
-          let(:expected_stats) do
-            stats = JSON.parse(body, symbolize_names: true)
-            stats[:apis_usage][period.to_sym][:by_endpoint].first
+        context 'when clicking on a tab' do
+          subject do
+            visit token_stats_path(token)
+
+            click_on("tabpanel-#{period}")
           end
 
-          it 'displays the corresponding period' do
-            subject
+          context 'for the last 10 minutes period' do
+            let(:period) { 'last_10_minutes' }
 
-            expect(page).to have_table("calls_ratio_#{period}", with_rows: [])
+            it 'displays the corresponding period (no stats for 10mn in fixtures file)' do
+              subject
+
+              expect(page).to have_table('calls_ratio_last_10_minutes', with_rows: [])
+            end
+
+            it 'does not display stats from other periods' do
+              subject
+
+              expect(page).not_to have_table('calls_ratio_last_8_hours')
+            end
           end
 
-          it 'does not display stats from other periods' do
-            subject
+          context 'for the last 30 hours period' do
+            let(:period) { 'last_30_hours' }
 
-            expect(page).not_to have_table('calls_ratio_last_10_minutes')
-          end
+            it 'displays the corresponding period (no stats for 30h in fixtures file)' do
+              subject
 
-          it 'displays data in the right format' do
-            subject
+              expect(page).to have_table("calls_ratio_#{period}", with_rows: [])
+            end
 
-            expect(page).to have_table("calls_ratio_#{period}", with_rows: [
-              [
-                expected_stats[:name],
-                expected_stats[:total],
-                expected_stats[:percent_success],
-                expected_stats[:percent_not_found],
-                expected_stats[:percent_other_client_errors],
-                expected_stats[:percent_server_errors],
-              ]
-            ])
+            it 'does not display stats from other periods' do
+              subject
+
+              expect(page).not_to have_table('calls_ratio_last_10_minutes')
+            end
           end
         end
       end
-    end
 
-    describe 'last calls details' do
-      let(:one_of_the_last_calls) do
-        stats = JSON.parse(body, symbolize_names: true)
-        stats[:last_calls].first
-      end
+      describe 'last calls details' do
+        let(:one_of_the_last_calls) do
+          stats = JSON.parse(body, symbolize_names: true)
+          stats[:last_calls].first
+        end
 
-      it 'displays the details of the last few calls' do
-        subject
+        it 'displays the details of the last few calls' do
+          subject
 
-        expect(page).to have_table('requests_details', with_rows: [
-          [
-            one_of_the_last_calls[:url],
-            one_of_the_last_calls[:params].to_s,
-            one_of_the_last_calls[:code],
-          ]
-        ])
+          expect(page).to have_table('requests_details', with_rows: [
+            [
+              one_of_the_last_calls[:url],
+              one_of_the_last_calls[:params].to_s,
+              one_of_the_last_calls[:code],
+            ]
+          ])
+        end
       end
     end
-  end
 
-  context 'when backend for stats does not work' do
-    let(:url) { "https://dashboard.entreprise.api.gouv.fr/api/watchdoge/stats/jwt_usage/#{token.id}" }
+    context 'when backend for stats does not work' do
+      let(:url) { "https://dashboard.entreprise.api.gouv.fr/api/watchdoge/stats/jwt_usage/#{token.id}" }
 
-    let(:stubbed_request) do
-      stub_request(:get, url).to_timeout
+      let(:stubbed_request) do
+        stub_request(:get, url).to_timeout
+      end
+
+      it_behaves_like :display_alert, :error
     end
-
-    it_behaves_like :display_alert, :error
   end
 end
