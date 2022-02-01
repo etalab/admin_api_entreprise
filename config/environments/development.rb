c880a551585195a89b60dde04bb860bcb9482226
Loïc Delmaire <loic.delmaire@gmail.com>
2022-02-01 16:52:00 +0000

    Configure a switch for dashboard/v3-beta

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,9 @@
 require 'active_support/core_ext/integer/time'
 
 Rails.application.configure do
+    config.hosts << "dashboard.entreprise.api.gouv.local"
+    config.hosts << "v3-beta.entreprise.api.gouv.local"
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -6,6 +6,8 @@ require "active_support/core_ext/integer/time"
 # and recreated between test runs. Don't rely on the data there!
 
 Rails.application.configure do
+  config.hosts = []
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   config.cache_classes = false
