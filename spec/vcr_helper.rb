6b1edf72617171e2ec0f77906488cc5758b20ac3
Samuelfaure <samuel.faure.dev@gmail.com>
2022-04-08 15:33:50 +0200

    fixing VCR

--- spec/vcr_helper.rb
+++ spec/vcr_helper.rb
@@ -6,6 +6,7 @@ VCR.configure do |c|
   c.hook_into :webmock
   c.configure_rspec_metadata!
   c.ignore_localhost = true
+  c.allow_http_connections_when_no_cassette = true
 
   c.filter_sensitive_data('<OAUTH_API_GOUV_CLIENT_SECRET>') { Rails.application.credentials.oauth_api_gouv_client_secret.to_s }
   c.filter_sensitive_data('<ELASTIC_SERVER_DOMAIN>') { Rails.application.credentials.elastic_server_domain.to_s }
