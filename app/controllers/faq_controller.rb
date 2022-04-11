89271274947b580c37d342a2df982322d185c603
Loïc Delmaire <loic.delmaire@gmail.com>
2022-04-11 10:37:32 +0200

    Scout commit for var name in FAQController

--- app/controllers/faq_controller.rb
+++ app/controllers/faq_controller.rb
@@ -1,5 +1,5 @@
 class FAQController < ApplicationController
   def index
-    @faq_entries_grouped_by_entries = FAQEntry.grouped_by_categories
+    @faq_entries_grouped_by_categories = FAQEntry.grouped_by_categories
   end
 end
