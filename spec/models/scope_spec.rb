4584d0d593ca3aebad02b3c1d82e88e2b2dc920c
Alexis Leclerc <alexis@alxs.fr>
2022-06-06 16:28:15 +0200

    Keep Scopes in /data
    with specs

--- spec/models/scope_spec.rb
+++ spec/models/scope_spec.rb
@@ -12,4 +12,18 @@ RSpec.describe Scope do
       expect(build(:scope, api: 'wrong API')).not_to be_valid
     end
   end
+
+  describe 'data/scopes/*.yml' do
+    it 'validates scopes for API Particulier' do
+      scopes = YAML.load_file(Rails.root.join('config/data/scopes/particulier.yml'))
+      expect(scopes).to all have_key('code')
+      expect(scopes).to all have_key('name')
+    end
+
+    it 'validates scopes for API Entreprise' do
+      scopes = YAML.load_file(Rails.root.join('config/data/scopes/entreprise.yml'))
+      expect(scopes).to all have_key('code')
+      expect(scopes).to all have_key('name')
+    end
+  end
 end
