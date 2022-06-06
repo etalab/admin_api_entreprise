53923ce7b0326387c091a3d4176c30bef4140d7d
Loïc Delmaire <loic.delmaire@gmail.com>
2022-06-06 11:56:36 +0200

    Sidekiq UI is password protected
    Better to be password protected here than nginx config

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   require 'sidekiq/web'
   require 'sidekiq/cron/web'
 
+  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+    ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username), ::Digest::SHA256.hexdigest(Rails.application.credentials.sidekiq_ui_username)) &
+        ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(Rails.application.credentials.sidekiq_ui_password))
+  end
   mount Sidekiq::Web => '/sidekiq'
 
   namespace :api do
