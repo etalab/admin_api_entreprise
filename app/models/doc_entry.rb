6f3dc8c6c143e0a4e2c28746f9dc507d6697b49d
Samuelfaure <samuel.faure.dev@gmail.com>
2022-06-10 15:40:08 +0200

    Harmonizing names

--- app/models/doc_entry.rb
+++ app/models/doc_entry.rb
@@ -9,29 +9,12 @@ class DocEntry
 
   attr_accessor :section, :title, :content
 
-  # algoliasearch_active_model do
-  #   attributes :title, :content, :answer_markdownify
-
-  #   searchableAttributes %w[
-  #     title
-  #     content
-  #     answer_markdownify
-  #   ]
-  # end
-
   def self.developers
     I18n.t('doc_entries.sections.developers').map { |entry| new(title: entry[:title], content: entry[:content]) }
   end
 
   def self.guide_migration
     I18n.t('doc_entries.sections.guide_migration').map { |entry| new(title: entry[:title], content: entry[:content]) }
-  end
-  # def self.all
-  #   I18n.t('doc.sections').map { |entry| new(content: entry[:content]) }
-  # end
-
-  def self.find(slug)
-    # TODO
   end
 
   def content_markdownify
