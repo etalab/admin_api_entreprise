73547cbb68f118590265e94de40764de60d1c09c
Samuelfaure <samuel.faure.dev@gmail.com>
2022-06-13 15:39:06 +0200

    Rename DocEntry in DocumentationEntry

--- app/models/documentation_entry.rb
+++ app/models/documentation_entry.rb
@@ -1,20 +1,17 @@
 # frozen_string_literal: true
 
-require 'kramdown'
-require 'kramdown-parser-gfm'
-
-class DocEntry
+class DocumentationEntry
   include ActiveModel::Model
   include ActiveModelAlgoliaSearchable
 
   attr_accessor :section, :title, :content
 
   def self.developers
-    I18n.t('doc_entries.sections.developers').map { |entry| new(title: entry[:title], content: entry[:content]) }
+    I18n.t('documentation_entries.sections.developers').map { |entry| new(title: entry[:title], content: entry[:content]) }
   end
 
   def self.guide_migration
-    I18n.t('doc_entries.sections.guide_migration').map { |entry| new(title: entry[:title], content: entry[:content]) }
+    I18n.t('documentation_entries.sections.guide_migration').map { |entry| new(title: entry[:title], content: entry[:content]) }
   end
 
   def anchor
