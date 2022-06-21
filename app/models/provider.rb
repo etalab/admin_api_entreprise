34b31c477eed3d7fcfff500ffe0ac23bca934f92
Samuelfaure <samuel.faure.dev@gmail.com>
2022-06-21 12:19:33 +0200

    Added links to providers on homepage

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -1,11 +1,9 @@
 class Provider
   include ActiveModel::Model
 
-  attr_accessor :slug, :name
+  attr_accessor :slug, :name, :link
 
   def self.all
-    I18n.t('providers').map do |slug, name|
-      new(slug:, name:)
-    end
+    I18n.t('providers').map { |entry| new(entry) }
   end
 end
