9084c16ff2ac8129a3bc159caf3267e2d865ad0b
Samuelfaure <samuel.faure.dev@gmail.com>
2022-06-29 16:29:07 +0200

    Added dynamic buttons to copy anchors in FAQ

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,5 +3,4 @@
 //= require_tree .
 
 //= require utils/table_search
-//= require utils/copy_anchor
 //= stub utils/generate_anchor_icons
--- app/assets/javascripts/utils/copy_anchor.js
+++
@@ -1,6 +0,0 @@
-function copyAnchor(anchor) {
-  const pageUrlNoAnchor = document.URL.replace(/#.*$/, "")
-  const url = pageUrlNoAnchor + '#' + anchor
-
-  navigator.clipboard.writeText(url)
-}
