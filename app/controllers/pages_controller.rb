fc1058935c70a6acc66c4734d3d235135663e661
Samuelfaure <samuel.faure.dev@gmail.com>
2022-07-10 11:39:26 +0200

    post-review fixes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   helper AlgoliaQueryHelper
+
   layout 'no_container', only: %i[home]
 
   def current_status
--- app/helpers/algolia_query_helper.rb
+++ app/helpers/algolia_query_helper.rb
@@ -1,5 +1,11 @@
 module AlgoliaQueryHelper
-  def query_filter_providers(slug)
-    "?Endpoint[query]=#{slug}"
+  include Rails.application.routes.url_helpers
+
+  def endpoints_with_provider_query_filter_path(provider)
+    endpoints_path + query_filter_providers(provider)
+  end
+
+  def query_filter_providers(provider)
+    "?Endpoint[query]=#{provider.slug}"
   end
 end
