bc87b9537e8ba4345ae4e37308185a95a42686f7
Loïc Delmaire <loic.delmaire@gmail.com>
2022-07-21 15:44:07 +0200

    Bugfix: harden autoOpenAccordeon if come from another page
    Basically handles turbo lifecyle (and some boyscouting on too generic
    naming)

--- app/assets/javascripts/utils/auto_open_accordeon.js
+++ app/assets/javascripts/utils/auto_open_accordeon.js
@@ -1,31 +1,36 @@
-window.addEventListener('load', function() {
-  init()
+['turbo:render', 'turbo:load'].forEach(function(e) {
+  document.addEventListener(e, function() {
+    initAutoOpenAccordeon()
 
-  function init() {
-    const accordeons = document.getElementsByClassName('fr-accordion')
+    function initAutoOpenAccordeon() {
+      const accordeons = document.getElementsByClassName('fr-accordion')
 
-    if (accordeons.length == 0)
-      return
+      if (accordeons.length == 0)
+        return
 
-    if (window.location.hash) {
-      const hash = window.location.hash
+      if (window.location.hash) {
+        const hash = window.location.hash
 
-      openDetails(hash)
-    }
-  }
-
-  function openDetails(hash) {
-    const target = document.getElementById(decodeURIComponent(hash.substring(1)))
-
-    if (target) {
-      const accordeon = target.querySelector('.fr-accordion__btn')
-
-      if (accordeon) {
-        setTimeout(function() {
-          accordeon.setAttribute('aria-expanded', 'true');
-          target.scrollIntoView()
-        }, 100);
+        _openAccordeonDetails(hash)
       }
     }
-  }
-})
+
+    function _openAccordeonDetails(hash) {
+      const target = document.getElementById(decodeURIComponent(hash.substring(1)))
+
+      if (target) {
+        const accordeon = target.querySelector('.fr-accordion__btn')
+
+        if (accordeon) {
+          setTimeout(function() {
+            accordeon.setAttribute('aria-expanded', 'true');
+          }, 100);
+
+          setTimeout(function() {
+            target.scrollIntoView()
+          }, 300);
+        }
+      }
+    }
+  })
+});
