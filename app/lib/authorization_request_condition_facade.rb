d4b1e6930d6cb466d2d1999f732fa0674957138d
Loïc Delmaire <loic.delmaire@gmail.com>
2022-10-25 14:22:41 +0200

    DataPass email validation: distinct email if editeur
    Send an only email on validation to demandeur only
    
    Closes https://github.com/etalab/admin_api_entreprise/issues/719

--- app/lib/authorization_request_condition_facade.rb
+++ app/lib/authorization_request_condition_facade.rb
@@ -1,27 +1,39 @@
 class AuthorizationRequestConditionFacade < SimpleDelegator
-  def all_contacts_have_different_emails?
-    [
-      user.email,
-      contact_technique.email,
-      contact_metier.email
-    ].uniq.count == 3
+  def not_editor_and_all_contacts_have_different_emails?
+    not_editor_authorization_request &&
+      [
+        user.email,
+        contact_technique.email,
+        contact_metier.email
+      ].uniq.count == 3
   end
 
-  def all_contacts_have_the_same_email?
-    [
-      user.email,
-      contact_technique.email,
-      contact_metier.email
-    ].uniq.count == 1
+  def not_editor_and_all_contacts_have_the_same_email?
+    not_editor_authorization_request &&
+      [
+        user.email,
+        contact_technique.email,
+        contact_metier.email
+      ].uniq.count == 1
   end
 
-  def user_is_contact_technique_and_not_contact_metier?
-    user.email == contact_technique.email &&
+  def not_editor_and_user_is_contact_technique_and_not_contact_metier?
+    not_editor_authorization_request &&
+      user.email == contact_technique.email &&
       user.email != contact_metier.email
   end
 
-  def user_is_contact_metier_and_not_contact_technique?
-    user.email == contact_metier.email &&
+  def not_editor_and_user_is_contact_metier_and_not_contact_technique?
+    not_editor_authorization_request &&
+      user.email == contact_metier.email &&
       user.email != contact_technique.email
+  end
+
+  def editor_authorization_request
+    demarche == 'editeurs'
+  end
+
+  def not_editor_authorization_request
+    !editor_authorization_request
   end
 end
