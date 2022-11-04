ed6f3e3fd2b4f0dce9a2847b1e2bb4c99ead13c0
Samuelfaure <samuel.faure.dev@gmail.com>
2022-11-04 11:34:21 +0100

    better token_mailer

--- app/mailers/token_mailer.rb
+++ app/mailers/token_mailer.rb
@@ -1,6 +1,10 @@
 class TokenMailer < ApplicationMailer
+  include FriendlyDateHelper
+
   def magic_link(magic_link)
     @magic_link_url = Rails.configuration.token_magic_link_url + magic_link.access_token
+    @tokens = magic_link.tokens
+    @expires_at = friendly_format_from_timestamp(magic_link.expires_at)
 
     to = magic_link.email
     subject = t('.subject')
