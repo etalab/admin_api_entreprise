095cd9e93b1b5556b170e81bbae5ac4d6d3948d5
Samuelfaure <samuel.faure.dev@gmail.com>
2022-11-16 14:17:42 +0100

    Introducing views for MagicLink connect

--- spec/features/api_entreprise/access_spec.rb
+++ spec/features/api_entreprise/access_spec.rb
@@ -2,10 +2,19 @@ require 'rails_helper'
 
 RSpec.describe 'page access', app: :api_entreprise do
   context 'when the user is not logged in' do
-    it 'can access the login page' do
-      visit login_path
 
+    before { visit login_path }
+
+    it 'can access the login page' do
       expect(page).to have_current_path(login_path, ignore_query: true)
+    end
+
+    it 'has a form to connect via datapass' do
+      expect(page).to have_button('datapass_login')
+    end
+
+    it 'has a link to connect via magic link' do
+      expect(page).to have_link('magic_link_login', href: login_magic_link_path)
     end
   end
 
