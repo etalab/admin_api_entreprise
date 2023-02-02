507f5e469f10621219be6a6b2f400b08d8f031f7
Loïc Delmaire <loic.delmaire@gmail.com>
2023-02-02 11:45:22 +0100

    Harden Sidekiq load config
    Without aliases: true it didn't work.
    Remove method and load directly config in order to prevent
    test to run if something's wrong

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,17 +1,13 @@
-def load_sidekiq_cron_jobs
-  schedule_file = Rails.root.join('config/schedule.yml')
+schedule_file = Rails.root.join('config/schedule.yml')
 
-  if File.exist?(schedule_file)
-    loaded_conf = YAML.load_file(schedule_file)
-    Sidekiq::Cron::Job.load_from_hash(loaded_conf[Rails.env])
-  end
+if File.exist?(schedule_file)
+  loaded_conf = YAML.load_file(schedule_file, aliases: true)
 end
-
 
 Sidekiq.configure_server do |config|
   config.redis = { url: ENV.fetch('REDIS_URL') { Rails.configuration.redis_database } }
 
-  load_sidekiq_cron_jobs
+  Sidekiq::Cron::Job.load_from_hash(loaded_conf[Rails.env])
 end
 
 Sidekiq.configure_client do |config|
