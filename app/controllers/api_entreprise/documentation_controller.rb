aed2726e1c4f4c883eba50ac7da965c76120577e
Loïc Delmaire <loic.delmaire@gmail.com>
2023-03-21 11:27:29 +0100

    Simplify models for documentation search
    Migrate DocumentationEntry to DocumentationPage, and simplify this model
    by using something more model oriented (it was instanciated as an array
    before, which is strange).
    
    Each DocumentationPage has multiple DocumentationSearchableChunk
    (inside sections: introduction and subsections), with the same model's
    attributes (title/content).

--- app/controllers/api_entreprise/documentation_controller.rb
+++ app/controllers/api_entreprise/documentation_controller.rb
@@ -2,13 +2,13 @@ class APIEntreprise::DocumentationController < APIEntrepriseController
   layout 'api_entreprise/no_container'
 
   def developers
-    @documentation_entries = DocumentationEntry.developers
+    @documentation_page = DocumentationPage.find('developers')
 
     render 'index'
   end
 
   def guide_migration
-    @documentation_entries = DocumentationEntry.guide_migration
+    @documentation_page = DocumentationPage.find('guide_migration')
 
     render 'index'
   end
---
+++ app/models/documentation_page.rb
@@ -0,0 +1,30 @@
+class DocumentationPage
+  include ActiveModel::Model
+
+  attr_accessor :title,
+    :sections
+
+  def id
+    uid
+  end
+
+  def self.find(uid)
+    page_data = I18n.t!("api_entreprise.documentation_pages.#{uid}")
+
+    new(
+      title: page_data[:title],
+      sections: page_data[:sections].map { |section| build_section(section, uid) }
+    )
+  rescue I18n::MissingTranslationData
+    raise ActiveRecord::RecordNotFound, "Documentation page '#{uid}' does not exist"
+  end
+
+  def self.build_section(section, page_uid)
+    {
+      introduction: DocumentationSearchableChunk.new(section, page_uid),
+      subsections: (section[:subsections] || []).map do |subsection|
+        DocumentationSearchableChunk.new(subsection, page_uid)
+      end
+    }
+  end
+end
