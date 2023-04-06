e31b4eccaf33932924998acbdee65da157eea945
Loïc Delmaire <loic.delmaire@gmail.com>
2023-04-06 11:56:11 +0200

    Enhance pages tests

--- spec/features/api_particulier/pages_spec.rb
+++ spec/features/api_particulier/pages_spec.rb
@@ -25,6 +25,14 @@ RSpec.describe 'Simple pages', app: :api_particulier do
     end
   end
 
+  describe 'accessibilite' do
+    it 'does not raise error' do
+      expect {
+        visit accessibilite_path
+      }.not_to raise_error
+    end
+  end
+
   describe 'account' do
     it 'does not raise error' do
       expect {
