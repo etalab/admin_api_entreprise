b0b34edf374e0a30804edf4072b529ff595b4517
Loïc Delmaire <loic.delmaire@gmail.com>
2023-04-25 18:20:37 +0200

    Refactor endpoint historic model logic

--- app/models/api_particulier/endpoint.rb
+++ app/models/api_particulier/endpoint.rb
@@ -11,24 +11,6 @@ class APIParticulier::Endpoint < AbstractEndpoint
     false
   end
 
-  def new_endpoints
-    return [] if !deprecated? || @new_endpoint_uids.blank?
-
-    @new_endpoint_uids.map do |new_endpoint_uid|
-      self.class.find(new_endpoint_uid)
-    end
-  end
-  
-  def old_endpoints
-    @old_endpoints ||= (@old_endpoint_uids || []).map do |old_endpoint_uid|
-      self.class.find(old_endpoint_uid)
-    end
-  end
-
-  def historicized?
-    old_endpoints.any?
-  end
-
   def extract_data_from_schema
     data_attributes_to_dig = %w[responses 200 content application/json schema properties]
     open_api_definition.dig(*data_attributes_to_dig)
