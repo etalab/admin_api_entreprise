d2339e48f577a7c09847d60644aa2c6782c800c5
Samuelfaure <samuel.faure.dev@gmail.com>
2023-05-09 12:17:05 +0200

    Adding feature test to login contact/user

--- spec/features/api_entreprise/access_spec.rb
+++ spec/features/api_entreprise/access_spec.rb
@@ -10,14 +10,28 @@ RSpec.describe 'page access', app: :api_entreprise do
   end
 
   context 'when the user is logged in' do
-    let(:user) { create(:user) }
+    describe 'when user is demandeur' do
+      let(:user) { create(:user, :demandeur) }
 
-    before { login_as(user) }
+      before { login_as(user) }
 
-    it 'is redirected to the user details while accessing the login page' do
-      visit login_path
+      it 'is redirected to the user details while accessing the login page' do
+        visit login_path
 
-      expect(page).to have_current_path(user_profile_path, ignore_query: true)
+        expect(page).to have_current_path(user_profile_path, ignore_query: true)
+      end
+    end
+
+    describe 'non-regression test: when user is contact_technique' do
+      let(:user) { create(:user, :contact_metier) }
+
+      before { login_as(user) }
+
+      it 'is redirected to the user details while accessing the login page' do
+        visit login_path
+
+        expect(page).to have_current_path(user_profile_path, ignore_query: true)
+      end
     end
   end
 end
