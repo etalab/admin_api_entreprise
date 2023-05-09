96246e7a2b6bda221a29d010ec6074f0f96d5ab0
Samuelfaure <samuel.faure.dev@gmail.com>
2023-05-09 15:02:52 +0200

    fix migration issue on staging2

--- db/migrate/20230316095426_merge_contacts_into_users.rb
+++ db/migrate/20230316095426_merge_contacts_into_users.rb
@@ -62,9 +62,9 @@ class MergeContactsIntoUsers < ActiveRecord::Migration[7.0]
 
       role = 'demandeur' if user.persisted?
 
-      UserAuthorizationRequestRole.create!(user:, authorization_request: contact.authorization_request, role:) if contact.authorization_request
-
       user.save
+
+      UserAuthorizationRequestRole.find_or_create_by(user:, authorization_request: contact.authorization_request, role:) if contact.authorization_request
     end
 
     remove_column :authorization_requests, :user_id
