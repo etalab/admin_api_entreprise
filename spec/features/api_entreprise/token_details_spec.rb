d719e96269de567c86543f26d0500a6cc76aaf00
Samuelfaure <samuel.faure.dev@gmail.com>
2023-05-10 10:04:14 +0200

    enhance contact permissions on tokens

--- spec/features/api_entreprise/token_details_spec.rb
+++ spec/features/api_entreprise/token_details_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe 'token details page', app: :api_entreprise do
-  let(:user) { create(:user, :with_token) }
-  let(:authorization_request) { create(:authorization_request, :with_demandeur, demandeur: user) }
+  let(:demandeur) { create(:user, :with_token) }
+  let(:authorization_request) { create(:authorization_request, :with_demandeur, :with_contact_technique, demandeur:) }
   let(:token) { create(:token, authorization_request:) }
 
   before do
@@ -10,49 +10,55 @@ RSpec.describe 'token details page', app: :api_entreprise do
     visit token_path(token)
   end
 
-  it 'displays tokens intitules' do
-    expect(page).to have_content(token.intitule)
+  shared_examples 'it can see/share tokens' do
+    it 'displays tokens intitules' do
+      expect(page).to have_content(token.intitule)
+    end
+
+    it 'displays tokens creation date' do
+      expect(page).to have_content(friendly_format_from_timestamp(token.iat))
+    end
+
+    it 'displays tokens expiration date' do
+      expect(page).to have_content(friendly_format_from_timestamp(token.exp))
+    end
+
+    it 'has a button to copy active tokens hash to clipboard' do
+      expect(page).to have_css("##{dom_id(token, :copy_button)}")
+    end
+
+    it 'displays tokens access scopes with humanized missing key' do
+      token = create(:token, :with_scopes, authorization_request:)
+      visit token_path(token)
+
+      expect(page).to have_content(*token.scopes.map(&:humanize))
+    end
+
+    it 'displays tokens access scopes with translations' do
+      token = create(:token, scopes: ['entreprises'], authorization_request:)
+      visit token_path(token)
+
+      expect(page).to have_content('INSEE Entreprise')
+    end
+
+    it 'has a link to access token stats' do
+      expect(page).to have_link(href: token_stats_path(token))
+    end
+
+    it 'has a link to the token contacts' do
+      expect(page).to have_link(href: token_contacts_path(token))
+    end
+
+    it 'has a button to create a magic link' do
+      expect(page).to have_button(dom_id(token, :modal_button))
+    end
   end
 
-  it 'displays tokens creation date' do
-    expect(page).to have_content(friendly_format_from_timestamp(token.iat))
-  end
+  context 'when user is demandeur' do
+    let(:user) { demandeur }
 
-  it 'displays tokens expiration date' do
-    expect(page).to have_content(friendly_format_from_timestamp(token.exp))
-  end
+    it_behaves_like 'it can see/share tokens'
 
-  it 'has a button to copy active tokens hash to clipboard' do
-    expect(page).to have_css("##{dom_id(token, :copy_button)}")
-  end
-
-  it 'displays tokens access scopes with humanized missing key' do
-    token = create(:token, :with_scopes, authorization_request:)
-    visit token_path(token)
-
-    expect(page).to have_content(*token.scopes.map(&:humanize))
-  end
-
-  it 'displays tokens access scopes with translations' do
-    token = create(:token, scopes: ['entreprises'], authorization_request:)
-    visit token_path(token)
-
-    expect(page).to have_content('INSEE Entreprise')
-  end
-
-  it 'has a button to create a magic link' do
-    expect(page).to have_button(dom_id(token, :modal_button))
-  end
-
-  it 'has a link to access token stats' do
-    expect(page).to have_link(href: token_stats_path(token))
-  end
-
-  it 'has a link to the token contacts' do
-    expect(page).to have_link(href: token_contacts_path(token))
-  end
-
-  context 'when the token has an authorization request' do
     it 'has a link for token renewal' do
       expect(page).to have_link(href: token_renew_path(token))
     end
@@ -62,13 +68,17 @@ RSpec.describe 'token details page', app: :api_entreprise do
     end
   end
 
-  context 'when connected as a simple user' do
-    it 'has no button to archive tokens' do
-      expect(page).not_to have_button(dom_id(token, :archive_button))
+  context 'when user is contact' do
+    let(:user) { authorization_request.contact_technique }
+
+    it_behaves_like 'it can see/share tokens'
+
+    it 'doesnt have a link for token renewal' do
+      expect(page).not_to have_link(href: token_renew_path(token))
     end
 
-    it 'has no button to blacklist tokens' do
-      expect(page).not_to have_button(dom_id(token, :blacklist_button))
+    it 'doesnt have a link to authorization request' do
+      expect(page).not_to have_link(href: datapass_authorization_request_url(token.authorization_request))
     end
   end
 end
