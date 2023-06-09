ff31799187727e3ae8acb019a4930e6010164ba2
Loïc Delmaire <loic.delmaire@gmail.com>
2023-06-09 15:43:46 +0200

    Some refactor for FAQ

--- config/sitemap/api-particulier.rb
+++ config/sitemap/api-particulier.rb
@@ -2,5 +2,4 @@ SitemapGenerator::Sitemap.default_host = "https://particulier.api.gouv.fr"
 SitemapGenerator::Sitemap.sitemaps_path = "sitemaps/api-particulier"
 SitemapGenerator::Sitemap.create do
   add api_particulier_stats_path
-  add faq_index_path
 end
