130bbfa84161e8be621880693aafcbbcd058a34e
Thomas COMES <thomas.comes.tech@gmail.com>
2023-06-14 12:21:48 +0200

    Added the qualifelec documentation

--- app/services/api_entreprise/open_api_definition.rb
+++ app/services/api_entreprise/open_api_definition.rb
@@ -8,6 +8,8 @@ class APIEntreprise::OpenAPIDefinition < AbstractOpenAPIDefinition
   def remote_url
     if Rails.env.sandbox?
       'https://sandbox.entreprise.api.gouv.fr/v3/openapi-entreprise.yaml'
+    elsif Rails.env.staging? || Rails.env.development?
+      'https://staging.entreprise.api.gouv.fr/v3/openapi-entreprise.yaml'
     else
       'https://entreprise.api.gouv.fr/v3/openapi-entreprise.yaml'
     end
