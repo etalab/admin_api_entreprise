9577084664b21087b1e3171db5ab2aa46ffdb199
Loïc Delmaire <loic.delmaire@gmail.com>
2023-07-20 16:47:22 +0200

    DatapassWebhook::APIParticulier: do not persist empty legacy_token_id

--- app/organizers/datapass_webhook/api_particulier.rb
+++ app/organizers/datapass_webhook/api_particulier.rb
@@ -4,7 +4,7 @@ module DatapassWebhook
       context.token_create_extra_params = {
         extra_info: {
           'legacy_token_id' => context.data['external_token_id']
-        }
+        }.compact
       }
 
       context.api = 'particulier'
