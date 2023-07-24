5af982e41e5292a7f9cbd613f106c400acea782a
Samuelfaure <samuel.faure.dev@gmail.com>
2023-07-24 10:32:32 +0200

    enabling capybara debug

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -42,7 +42,7 @@ require 'rack_session_access/capybara'
 require 'capybara/cuprite'
 Capybara.javascript_driver = :cuprite
 Capybara.register_driver(:cuprite) do |app|
-  Capybara::Cuprite::Driver.new(app, window_size: [1200, 800])
+  Capybara::Cuprite::Driver.new(app, window_size: [1200, 800], inspector: ENV.fetch('INSPECTOR', nil))
 end
 Capybara.server = :puma, { Silent: true }
 
