002b4570a752ba9eda617b12083426d97f96a9ea
Loïc Delmaire <loic.delmaire@gmail.com>
2023-07-26 11:40:22 +0200

    API Entreprise: introduce /stats.json

---
+++ spec/controllers/api_entreprise/stats_controller_spec.rb
@@ -0,0 +1,10 @@
+RSpec.describe APIEntreprise::StatsController do
+  describe 'GET #index as json' do
+    subject(:get_index) { get :index, params: { format: :json } }
+
+    before { get_index }
+
+    it { is_expected.to have_http_status(:success) }
+    it { expect(JSON.parse(response.body)).to be_an(Array) }
+  end
+end
