feb79050f5951283cb3bad5e9252629c9b49c3e0
Loïc Delmaire <loic.delmaire@gmail.com>
2023-08-31 15:53:05 +0200

    TokenExpirationNoticeJob can't send more than one email by batch
    Thanks to:
    
    1. Reorder of the scheduling
    2. Test on min element of already sent emails
    
    It is impossible to send more than one email by batch on a specific
    token.
    
    Closes https://linear.app/pole-api/issue/API-1055/harden-la-gestion-des-relances-sur-les-expirations

--- spec/jobs/token_expiration_notice_job_spec.rb
+++ spec/jobs/token_expiration_notice_job_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe TokenExpirationNoticeJob do
+  include ActiveJob::TestHelper
+
   subject { described_class.perform_now }
 
   shared_examples 'sending expiration notices' do |nb_days|
@@ -20,4 +22,30 @@ RSpec.describe TokenExpirationNoticeJob do
   it_behaves_like 'sending expiration notices', 30
   it_behaves_like 'sending expiration notices', 60
   it_behaves_like 'sending expiration notices', 90
+
+  describe 'when there is a token with an expiration date in 7 days' do
+    let!(:token) { create(:token, exp: 7.days.from_now, days_left_notification_sent:) }
+
+    before do
+      clear_enqueued_jobs
+    end
+
+    context 'when none of the expiration notice has been sent' do
+      let(:days_left_notification_sent) { [] }
+
+      it 'sent the notification for 7 days' do
+        expect {
+          subject
+        }.to have_enqueued_job(ScheduleExpirationNoticeMailjetEmailJob).with(token, 7)
+      end
+
+      it 'only sent one email' do
+        expect {
+          subject
+        }.to change(enqueued_jobs, :size).by(1)
+
+        expect(token.reload.days_left_notification_sent).to eq([7])
+      end
+    end
+  end
 end
