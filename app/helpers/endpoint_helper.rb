e05ef4f0617769d5a2e4ecf30660816e3c677009
Thomas COMES <thomas.comes.tech@gmail.com>
2023-09-11 10:37:00 +0200

    Ordered Catalog with pending status

--- app/helpers/endpoint_helper.rb
+++ app/helpers/endpoint_helper.rb
@@ -3,6 +3,13 @@ module EndpointHelper
     "<p class=\"fr-badge fr-badge--#{endpoint_status_color(endpoint)}\">#{endpoint_status_label(endpoint)}</p>".html_safe unless endpoint.pending_status.nil?
   end
 
+  def order_by_position_or_status(endpoint1, endpoint2)
+    return -1 if endpoint1.pending_status && !endpoint2.pending_status
+    return 1 if !endpoint1.pending_status && endpoint2.pending_status
+
+    0
+  end
+
   private
 
   def endpoint_status_color(endpoint)
