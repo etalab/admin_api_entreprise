8b1a03e0e9730a708b578814fc90438f211acf30
Loïc Delmaire <loic.delmaire@gmail.com>
2023-10-10 18:02:47 +0200

    Harden config for rails 7.1

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
-require 'omni_auth/strategies/api_gouv_abstract'
-require 'omni_auth/strategies/api_gouv_entreprise'
-require 'omni_auth/strategies/api_gouv_particulier'
+require_relative '../../app/lib/omni_auth/strategies/api_gouv_abstract.rb'
+require_relative '../../app/lib/omni_auth/strategies/api_gouv_entreprise.rb'
+require_relative '../../app/lib/omni_auth/strategies/api_gouv_particulier.rb'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :api_gouv_particulier, Rails.configuration.oauth_api_gouv_client_id_particulier, Rails.application.credentials.oauth_api_gouv_client_secret_particulier
