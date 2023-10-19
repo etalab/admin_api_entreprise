71940db721380ead724d8e3db2e011eeeaee5810
Samuelfaure <samuel.faure.dev@gmail.com>
2023-10-19 14:14:59 +0200

    disjonction host url for mailers depending on environment

--- app/mailers/api_entreprise_mailer.rb
+++ app/mailers/api_entreprise_mailer.rb
@@ -1,4 +1,5 @@
 class APIEntrepriseMailer < ApplicationMailer
   default from: 'support@entreprise.api.gouv.fr'
-  default_url_options[:host] = 'https://entreprise.api.gouv.fr'
+
+  default_url_options[:host] = Rails.env.production? ? 'https://entreprise.api.gouv.fr' : "https://#{Rails.env}.entreprise.api.gouv.fr"
 end
