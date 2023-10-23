11e6759e5f1f798bf4cb2432d2045be0ad525241
Thomas COMES <thomas.comes.tech@gmail.com>
2023-10-23 10:45:27 +0200

    Added friendly date formatter

--- app/helpers/friendly_date_helper.rb
+++ app/helpers/friendly_date_helper.rb
@@ -2,4 +2,8 @@ module FriendlyDateHelper
   def friendly_format_from_timestamp(timestamp)
     "#{Time.zone.at(timestamp).strftime('%d/%m/%Y à %Hh%M')} (heure de Paris)"
   end
+
+  def friendly_date_from_timestamp(timestamp)
+    Time.zone.at(timestamp).strftime('%d/%m/%Y').to_s
+  end
 end
