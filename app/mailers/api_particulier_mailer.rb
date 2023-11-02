a05b6b7173ca214b1af4eb8c5ad656fa59384428
Loïc Delmaire <loic.delmaire@gmail.com>
2023-11-02 15:05:17 +0100

    Mailer: refactor dynamic host

--- app/mailers/api_particulier_mailer.rb
+++ app/mailers/api_particulier_mailer.rb
@@ -2,4 +2,10 @@ class APIParticulierMailer < ApplicationMailer
   default from: 'API Particulier <support@particulier.api.gouv.fr>'
 
   default_url_options[:host] = Rails.env.production? ? 'https://particulier.api.gouv.fr' : "https://#{Rails.env}.particulier.api.gouv.fr"
+
+  protected
+
+  def extract_host
+    ActionMailer::Base.default_url_options[:host] = valid_host('api_particulier')
+  end
 end
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,4 +2,26 @@ class ApplicationMailer < ActionMailer::Base
   layout 'mailer'
 
   helper :friendly_date
+
+  before_action :extract_host
+
+  protected
+
+  def attach_logos_for(kind)
+    attachments.inline['logo-api.png'] = Rails.root.join("app/assets/images/mailers/#{kind}/header-logo-api.png").read
+    attachments.inline['logo-dinum.png'] = Rails.root.join('app/assets/images/mailers/header-logo-dinum.png').read
+  end
+
+  def valid_host(namespace)
+    api = namespace.split('_').last
+
+    case Rails.env
+    when 'production'
+      "#{api}.api.gouv.fr"
+    when 'test', 'development'
+      "#{api}.api.localtest.me:3000"
+    else
+      "#{Rails.env}.#{api}.api.gouv.fr"
+    end
+  end
 end
