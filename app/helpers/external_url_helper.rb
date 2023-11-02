1afaabfcef6d2afd81730019386333a7aaa67bfa
Loïc Delmaire <loic.delmaire@gmail.com>
2023-11-02 15:31:59 +0100

    Refactor DataPass urls management
    Make it DRY, more readable and solve invalid link for API Particulier

--- app/helpers/external_url_helper.rb
+++ app/helpers/external_url_helper.rb
@@ -1,9 +1,22 @@
 module ExternalUrlHelper
   def datapass_renewal_url(authorization_request)
-    "#{Rails.configuration.token_renewal_url}#{authorization_request.external_id}"
+    "#{datapass_base_url}/copy-authorization-request/#{authorization_request.external_id}"
   end
 
   def datapass_authorization_request_url(authorization_request)
-    "#{Rails.configuration.token_authorization_request_url}#{authorization_request.external_id}"
+    "#{datapass_base_url}/api-#{authorization_request.api}/#{authorization_request.external_id}"
+  end
+
+  private
+
+  def datapass_base_url
+    case Rails.env
+    when 'staging'
+      'https://staging.datapass.api.gouv.fr'
+    when 'sandbox', 'test', 'development'
+      'https://sandbox.datapass.api.gouv.fr'
+    else
+      'https://datapass.api.gouv.fr'
+    end
   end
 end
