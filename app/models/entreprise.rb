7344ad9afb1c1d73860c670dc24828731bd66b65
Loïc Delmaire <loic.delmaire@gmail.com>
2023-11-10 17:11:29 +0100

    Add factory for Entreprise
    Simplify tests

--- app/models/entreprise.rb
+++ app/models/entreprise.rb
@@ -13,4 +13,8 @@ class Entreprise
   def raison_sociale
     personne_morale_attributs[:raison_sociale]
   end
+
+  def naf
+    "#{activite_principale[:code]} - #{activite_principale[:libelle]}"
+  end
 end
