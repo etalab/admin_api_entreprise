c948d8d80b016b5a3cf506b44983d8dcd3757c60
Thomas COMES <thomas.comes.tech@gmail.com>
2023-12-05 08:44:16 +0100

    Fixing regression on APIPart templates
    Error occured when an authenticated user would open non authenticated
    APIPart pages

--- app/controllers/api_particulier/authenticated_users_controller.rb
+++ app/controllers/api_particulier/authenticated_users_controller.rb
@@ -1,9 +1,3 @@
 class APIParticulier::AuthenticatedUsersController < APIParticulierController
   include AuthenticatedUserManagement
-
-  helper_method :tokens_to_export?
-
-  def tokens_to_export?
-    TokenExport.new(current_user).tokens_to_export?
-  end
 end
