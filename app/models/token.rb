cac4bd777f8135ca12989b18503fc3600abda780
Thomas COMES <thomas.comes.tech@gmail.com>
2023-12-11 11:01:49 +0100

    Introduce used? method for Token model

--- app/models/token.rb
+++ app/models/token.rb
@@ -36,6 +36,10 @@ class Token < ApplicationRecord
     exp < Time.zone.now.to_i
   end
 
+  def used?
+    !access_logs.limit(1).empty?
+  end
+
   delegate :api, to: :authorization_request
 
   def self.default_create_params
