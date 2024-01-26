e0f53718bf071d1cfb6650cab2bc559e7c8c3c04
Loïc Delmaire <loic.delmaire@gmail.com>
2024-01-26 17:48:40 +0100

    Enhance cas usages feature tests

--- spec/features/api_particulier/cas_usages_spec.rb
+++ spec/features/api_particulier/cas_usages_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'Cas usages pages', app: :api_particulier do
   describe 'index' do
     it 'does not raise error' do
       expect {
-        visit cas_usages_path
+        visit api_particulier_cas_usages_path
       }.not_to raise_error
     end
   end
@@ -12,13 +12,13 @@ RSpec.describe 'Cas usages pages', app: :api_particulier do
   describe 'show' do
     it 'does not raise error' do
       APIParticulier::CasUsage.all.each do |cas_usage|
-        visit cas_usage_path(uid: cas_usage.uid)
-        expect(page).to have_current_path(cas_usage_path(cas_usage.uid), ignore_query: true)
+        visit api_particulier_cas_usage_path(uid: cas_usage.uid)
+        expect(page).to have_current_path(api_particulier_cas_usage_path(cas_usage.uid), ignore_query: true)
       end
     end
 
     it 'redirects to root path when cas_usage is not found' do
-      visit cas_usage_path(uid: '0123456789')
+      visit api_particulier_cas_usage_path(uid: '0123456789')
       expect(page).to have_current_path root_path
     end
   end
