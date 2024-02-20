1b845b9152e8dcb1753fff0c80e2051c3a5726d7
Loïc Delmaire <loic.delmaire@gmail.com>
2024-02-20 12:01:11 +0100

    Track users transferts

--- app/organizers/user/transfer_account.rb
+++ app/organizers/user/transfer_account.rb
@@ -2,4 +2,11 @@ class User::TransferAccount < ApplicationOrganizer
   organize User::FindOrCreateNewOwner,
     User::TransferTokens,
     User::NotifyNewTokensOwner
+
+  after do
+    Sentry.set_extras(
+      current_owner_id: context.current_owner.id
+    )
+    Sentry.capture_message('User::TransferAccount completed', level: :info)
+  end
 end
