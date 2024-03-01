a9a79495aa000f709a3e11e57ffef5f6d7ebc3b4
Dorine Lambinet <dorine.lambinet@gmail.com>
2024-03-01 18:05:47 +0100

    Supprime les éléments se référant au guide de migration

--- app/controllers/api_entreprise/documentation_controller.rb
+++ app/controllers/api_entreprise/documentation_controller.rb
@@ -6,10 +6,4 @@ class APIEntreprise::DocumentationController < APIEntrepriseController
 
     render 'index'
   end
-
-  def guide_migration
-    @documentation_page = APIEntreprise::DocumentationPage.find('guide_migration')
-
-    render 'index'
-  end
 end
