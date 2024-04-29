c2f8d63d5504e74212f4759cd53ee4382ae2153d
Samuelfaure <samuel.faure.dev@gmail.com>
2024-04-29 13:20:54 +0900

    Deprecated APIs are back in catalogue
    We only have CNAF QFv1 and we want it back, but
    keeping the "deprecated" tag

--- spec/features/api_particulier/endpoints/index_spec.rb
+++ spec/features/api_particulier/endpoints/index_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe 'Endpoints index', app: :api_particulier do
   it 'displays endpoints with basic info and link to show' do
     visit endpoints_path
 
-    expect(page).to have_css('.endpoint-card', count: APIParticulier::Endpoint.all.reject(&:deprecated?).count)
+    expect(page).to have_css('.endpoint-card', count: APIParticulier::Endpoint.all.count)
     expect(page).to have_css("##{dom_id(sample_endpoint)}")
 
     within("##{dom_id(sample_endpoint)}") do
