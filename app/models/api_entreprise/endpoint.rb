309d4e9e01052ed581ac8f4b8ed606b58dd85c16
Samuelfaure <samuel.faure.dev@gmail.com>
2024-08-06 11:41:31 +0700

    Fix error when endpoint missing
    We expect a hash to call "stringify_keys",
    or raise if nil (when value is absent).
    I18n returns "Translation missing" string if absent,
    which isn't caught by "nil?" and errors with "stringify_keys".

--- app/models/api_entreprise/endpoint.rb
+++ app/models/api_entreprise/endpoint.rb
@@ -38,7 +38,8 @@ class APIEntreprise::Endpoint < AbstractEndpoint
   end
 
   def load_dummy_definition!
-    missing_endpoints_definition = I18n.t("api_entreprise.missing_endpoints.#{path}")
+    missing_endpoints_definition = I18n.t("api_entreprise.missing_endpoints.#{path}", default: nil)
+
     raise 'Endpoint(s) not found, check endpoints paths are available in OpenAPI file or in missing_endpoints.yml' if missing_endpoints_definition.nil?
 
     @open_api_definition = missing_endpoints_definition.stringify_keys
