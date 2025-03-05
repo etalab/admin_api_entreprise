0007a0072b99065731da5ba443db20e77055d353
Delmaire Loïc <loic.delmaire@gmail.com>
2025-03-05 18:05:00 +0100

    Enhance MetabaseEmbedService interface
    Allow to pass resource instead of question (prepare to embed dashboards)

--- app/services/metabase_embed_service.rb
+++ app/services/metabase_embed_service.rb
@@ -3,27 +3,35 @@ require 'jwt'
 class MetabaseEmbedService
   METABASE_SITE_URL = 'https://metabase.entreprise.api.gouv.fr'.freeze
 
-  def initialize(question_id, params = {})
-    @question_id = question_id
+  def initialize(resource:, params: {})
+    @resource = resource
     @params = params
   end
 
   def url
-    "#{METABASE_SITE_URL}/embed/question/#{token}#bordered=false&titled=false"
+    "#{METABASE_SITE_URL}/embed/#{kind}/#{token}#bordered=false&titled=false"
   end
 
   private
 
   def token
     payload = {
-      resource: {
-        question: @question_id
-      },
+      resource: @resource,
       params: @params,
       exp: Time.now.to_i + (60 * 10)
     }
 
     JWT.encode(payload, metabase_secrey_key)
+  end
+
+  def kind
+    if @resource.key?(:question_id) || @resource.key?(:question)
+      'question'
+    elsif @resource.key?(:dashboard)
+      'dashboard'
+    else
+      raise ArgumentError, 'Invalid resource'
+    end
   end
 
   def metabase_secrey_key
