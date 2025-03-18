3b95c7f3d8515177374373f7494ba04a7af63002
Thomas COMES <thomas.comes.tech@gmail.com>
2025-03-18 11:34:34 +0100

    emoving references to legacy tokens files
    The file has been removed from the production environment and the code
    had to be adapted.
    
    Sentry issue for reference : https://errors.data.gouv.fr/organizations/sentry/issues/250743/?environment=production&query=is%3Aunresolved&referrer=issue-stream

--- app/controllers/api_particulier/new_tokens_controller.rb
+++
@@ -1,7 +0,0 @@
-require 'csv'
-
-class APIParticulier::NewTokensController < APIParticulier::AuthenticatedUsersController
-  def download
-    send_data TokenExport.new(current_user, impersonated: true_user != current_user).perform, filename: 'new_tokens.csv'
-  end
-end
