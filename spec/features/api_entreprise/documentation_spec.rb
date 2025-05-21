714e81c81e25298e50d8ce6d5cc0efdc69e1d902
Thomas COMES <thomas.comes.tech@gmail.com>
2025-05-21 15:39:49 +0200

    Unification of documentation specs

--- spec/features/api_entreprise/documentation_spec.rb
+++ spec/features/api_entreprise/documentation_spec.rb
@@ -1,13 +1,8 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
+require_relative '../../support/shared_examples/features/documentation'
 
 RSpec.describe 'Documentation pages', app: :api_entreprise do
-  describe 'developers' do
-    it 'does not raise error' do
-      expect {
-        visit developers_path
-      }.not_to raise_error
-    end
-  end
+  it_behaves_like 'a documentation feature'
 end
