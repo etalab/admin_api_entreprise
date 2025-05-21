51589032fa0c1547d30edf82f43536d2cd073de6
Thomas COMES <thomas.comes.tech@gmail.com>
2025-05-21 16:02:59 +0200

    Unification of pages specs

--- spec/features/api_particulier/pages_spec.rb
+++ spec/features/api_particulier/pages_spec.rb
@@ -1,80 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe 'Simple pages', app: :api_particulier do
-  describe 'home' do
-    it 'does not raise error' do
-      visit root_path
-
-      expect(page).to have_content('API Particulier')
-    end
-  end
-
-  describe 'mentions' do
-    it 'does not raise error' do
-      expect {
-        visit mentions_path
-      }.not_to raise_error
-    end
-  end
-
-  describe 'cgu' do
-    it 'does not raise error' do
-      expect {
-        visit cgu_path
-      }.not_to raise_error
-    end
-  end
-
-  describe 'donnees_personnelles' do
-    it 'does not raise error' do
-      expect {
-        visit donnees_personnelles_path
-      }.not_to raise_error
-    end
-  end
-
-  describe 'accessibilite' do
-    it 'does not raise error' do
-      expect {
-        visit accessibilite_path
-      }.not_to raise_error
-    end
-  end
-
-  describe 'infolettre' do
-    it 'does not raise error' do
-      expect {
-        visit newsletter_path
-      }.not_to raise_error
-    end
-
-    it 'does not mention API Entreprise' do
-      visit newsletter_path
-
-      expect(page).to have_content('API Particulier')
-      expect(page).to have_no_content('API Entreprise')
-    end
-  end
-
-  describe 'account' do
-    it 'does not raise error' do
-      expect {
-        visit '/compte'
-      }.not_to raise_error
-
-      expect(page).to have_content('API Particulier')
-    end
-  end
-
-  describe 'developers redoc page', :js do
-    it 'works and displays openapi container' do
-      Capybara.using_wait_time 5 do
-        expect {
-          visit developers_openapi_path
-        }.not_to raise_error
-
-        expect(page).to have_content('Quotient familial')
-      end
-    end
-  end
+  it_behaves_like 'static pages feature',
+    check_root_content: true,
+    check_newsletter_content: true,
+    check_account_page: true,
+    developers_content: 'Quotient familial',
+    expected_api_name: 'API Particulier',
+    unexpected_api_name: 'API Entreprise'
 end
---
+++ spec/support/shared_examples/features/pages.rb
@@ -0,0 +1,87 @@
+require 'rails_helper'
+
+RSpec.shared_examples 'static pages feature' do |options = {}|
+  # Pages common to both APIs
+  common_path_helpers = %i[mentions_path cgu_path donnees_personnelles_path accessibilite_path newsletter_path]
+
+  # Common pages tests
+  common_path_helpers.each do |path_helper|
+    describe path_helper.to_s.gsub('_path', '') do
+      it 'does not raise error' do
+        expect {
+          visit send(path_helper)
+        }.not_to raise_error
+      end
+    end
+  end
+
+  # Configure API-specific content checks
+  if options[:check_newsletter_content]
+    describe 'infolettre content' do
+      it "mentions #{options[:expected_api_name]} and not #{options[:unexpected_api_name]}" do
+        visit newsletter_path
+
+        expect(page).to have_content(options[:expected_api_name])
+        expect(page).to have_no_content(options[:unexpected_api_name])
+      end
+    end
+  end
+
+  # Optional root path check with API name
+  if options[:check_root_content]
+    describe 'home' do
+      it 'does not raise error and shows correct API name' do
+        visit root_path
+
+        expect(page).to have_content(options[:expected_api_name])
+      end
+    end
+  end
+
+  # Account page check if provided
+  if options[:check_account_page]
+    describe 'account' do
+      it 'does not raise error and shows correct API name' do
+        expect {
+          visit '/compte'
+        }.not_to raise_error
+
+        expect(page).to have_content(options[:expected_api_name])
+      end
+    end
+  end
+
+  # Optional developers page with custom content check
+  describe 'developers redoc page', :js do
+    it 'works and displays expected content' do
+      Capybara.using_wait_time 5 do
+        expect {
+          visit developers_openapi_path
+        }.not_to raise_error
+
+        expect(page).to have_content(options[:developers_content]) if options[:developers_content]
+      end
+    end
+  end
+
+  # Additional custom pages
+  options[:additional_pages]&.each do |page_config|
+    path_helper = page_config[:path_helper]
+    path = page_config[:path]
+    content = page_config[:content]
+
+    describe page_config[:name] do
+      it 'does not raise error' do
+        expect {
+          if path_helper
+            visit send(path_helper)
+          else
+            visit path
+          end
+        }.not_to raise_error
+
+        expect(page).to have_content(content) if content
+      end
+    end
+  end
+end
