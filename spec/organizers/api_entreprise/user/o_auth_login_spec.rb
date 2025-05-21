32f7166ba5999f385d2376b7df187076fcdf71b8
Thomas COMES <thomas.comes.tech@gmail.com>
2025-05-21 15:33:35 +0200

    Unification of o auth login specs

--- spec/organizers/api_entreprise/user/o_auth_login_spec.rb
+++ spec/organizers/api_entreprise/user/o_auth_login_spec.rb
@@ -1,71 +1,8 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
+require_relative '../../../support/shared_examples/organizers/user/o_auth_login'
 
 RSpec.describe APIEntreprise::User::OAuthLogin, type: :organizer do
-  subject(:sync!) { described_class.call(params) }
-
-  let(:params) do
-    {
-      oauth_api_gouv_email: user.email,
-      oauth_api_gouv_id:,
-      oauth_api_gouv_info:,
-      namespace: 'api_entreprise'
-    }
-  end
-
-  let(:oauth_api_gouv_info) do
-    OmniAuth::AuthHash::InfoHash.new(
-      email: user.email,
-      family_name: user.last_name,
-      given_name: user.first_name,
-      sub: user.oauth_api_gouv_id || unknown_api_gouv_id
-    )
-  end
-
-  let(:unknown_api_gouv_id) { '9001' }
-
-  context 'when the user does not exist' do
-    let(:user) { build(:user, :with_full_name) }
-    let(:oauth_api_gouv_id) { 'whatever' }
-
-    it { is_expected.to be_a_success }
-
-    it 'creates a user with valid attributes' do
-      expect {
-        subject
-      }.to change(User, :count).by(1)
-
-      latest_user = User.last
-
-      %w[email first_name last_name].each do |attr|
-        expect(latest_user.send(attr)).to eq(user.send(attr))
-      end
-    end
-  end
-
-  context 'when the user exists in the database' do
-    let!(:user) { create(:user) }
-    let(:oauth_api_gouv_id) { '1234' }
-
-    it { is_expected.to be_a_success }
-
-    it 'does not create a new user' do
-      expect {
-        sync!
-      }.not_to change(User, :count)
-    end
-
-    context 'when the user does not have a known API Gouv ID' do
-      let!(:user) { create(:user, oauth_api_gouv_id: nil) }
-      let(:oauth_api_gouv_id) { '1234' }
-
-      it { is_expected.to be_a_success }
-
-      it 'updates the OAuth API Gouv ID for the user' do
-        sync!
-        user.reload
-
-        expect(user.oauth_api_gouv_id).to eq(oauth_api_gouv_id)
-      end
-    end
-  end
+  it_behaves_like 'an oauth login organizer', 'api_entreprise'
 end
---
+++ spec/support/shared_examples/organizers/user/o_auth_login.rb
@@ -0,0 +1,70 @@
+# frozen_string_literal: true
+
+RSpec.shared_examples 'an oauth login organizer' do |namespace|
+  subject(:sync!) { described_class.call(params) }
+
+  let(:params) do
+    {
+      oauth_api_gouv_email: user.email,
+      oauth_api_gouv_id: oauth_api_gouv_id,
+      oauth_api_gouv_info: oauth_api_gouv_info,
+      namespace: namespace
+    }
+  end
+
+  let(:oauth_api_gouv_info) do
+    OmniAuth::AuthHash::InfoHash.new(
+      email: user.email,
+      family_name: user.last_name,
+      given_name: user.first_name,
+      sub: user.oauth_api_gouv_id || unknown_api_gouv_id
+    )
+  end
+
+  let(:unknown_api_gouv_id) { '9001' }
+
+  context 'when the user does not exist' do
+    let(:user) { build(:user, :with_full_name) }
+    let(:oauth_api_gouv_id) { 'whatever' }
+
+    it { is_expected.to be_a_success }
+
+    it 'creates a user with valid attributes' do
+      expect {
+        subject
+      }.to change(User, :count).by(1)
+
+      latest_user = User.last
+
+      %w[email first_name last_name].each do |attr|
+        expect(latest_user.send(attr)).to eq(user.send(attr))
+      end
+    end
+  end
+
+  context 'when the user exists in the database' do
+    let!(:user) { create(:user) }
+    let(:oauth_api_gouv_id) { user.oauth_api_gouv_id || '1234' }
+
+    it { is_expected.to be_a_success }
+
+    it 'does not create a new user' do
+      expect {
+        sync!
+      }.not_to change(User, :count)
+    end
+
+    context 'when the user does not have a known API Gouv ID' do
+      let!(:user) { create(:user, oauth_api_gouv_id: nil) }
+      let(:oauth_api_gouv_id) { '1234' }
+
+      it { is_expected.to be_a_success }
+
+      it 'updates the OAuth API Gouv ID for the user' do
+        sync!
+        user.reload
+        expect(user.oauth_api_gouv_id).to eq(oauth_api_gouv_id)
+      end
+    end
+  end
+end
