c78ce50613878fa81d2bf01e78f64dae0fdf557e
Delmaire Loïc <loic.delmaire@gmail.com>
2025-08-06 18:40:37 +0200

    Remove old omniauth config

--- app/lib/omni_auth/strategies/api_gouv_particulier.rb
+++
@@ -1,7 +0,0 @@
-module OmniAuth::Strategies
-  class APIGouvParticulier < OmniAuth::Strategies::APIGouvAbstract
-    option :name, :api_gouv_particulier
-  end
-end
-
-OmniAuth::Strategies::ApiGouvParticulier = OmniAuth::Strategies::APIGouvParticulier
