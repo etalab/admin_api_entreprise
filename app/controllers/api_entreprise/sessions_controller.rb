09703a4003b2695c823937b07447fbd9928dba21
Delmaire Loïc <loic.delmaire@gmail.com>
2025-08-07 08:20:56 +0200

    Clean old authenticate organizers

--- app/controllers/api_entreprise/sessions_controller.rb
+++ app/controllers/api_entreprise/sessions_controller.rb
@@ -1,9 +1,3 @@
 class APIEntreprise::SessionsController < APIEntrepriseController
   include SessionsManagement
-
-  protected
-
-  def oauth_login_organizer
-    APIEntreprise::User::OAuthLogin
-  end
 end
--- app/controllers/api_particulier/sessions_controller.rb
+++ app/controllers/api_particulier/sessions_controller.rb
@@ -1,9 +1,3 @@
 class APIParticulier::SessionsController < APIParticulierController
   include SessionsManagement
-
-  protected
-
-  def oauth_login_organizer
-    APIParticulier::User::OAuthLogin
-  end
 end
