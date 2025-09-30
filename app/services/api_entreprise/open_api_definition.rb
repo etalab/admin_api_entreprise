7fae3e46badc656fa917f72ce8f1f50af2da87df
Samuelfaure <samuel.faure.dev@gmail.com>
2025-09-30 13:47:30 +0200

    Fix: local definition of API Particulier endpoints v2 vs v3

--- app/services/api_entreprise/open_api_definition.rb
+++ app/services/api_entreprise/open_api_definition.rb
@@ -1,7 +1,7 @@
 class APIEntreprise::OpenAPIDefinition < AbstractOpenAPIDefinition
   protected
 
-  def local_path
+  def local_path(_url)
     Rails.root.join('config/api-entreprise-v3-openapi.yml')
   end
 
