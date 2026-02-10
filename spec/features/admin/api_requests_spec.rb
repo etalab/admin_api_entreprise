8051f49f9b7b2eee898138342e6ccbcd32945007
Delmaire Loïc <loic.delmaire@gmail.com>
2026-02-10 16:45:54 +0100

    Add context override and force request object metadata

--- spec/features/admin/api_requests_spec.rb
+++ spec/features/admin/api_requests_spec.rb
@@ -9,6 +9,14 @@ RSpec.describe 'Admin: API requests' do
 
   context 'when using API Entreprise', app: :api_entreprise do
     let(:siade_entreprise_url) { APIEntreprise::BASE_URL }
+    let(:default_object_value) { "user_#{admin.id}" }
+
+    def valid_api_entreprise_request?(req, context:, object:)
+      query = Rack::Utils.parse_nested_query(req.uri.query)
+      query['context'] == context &&
+        query['object'] == object &&
+        query['recipient'] == '13002526500013'
+    end
 
     describe 'visiting the API requests page' do
       it 'displays the API requests form with endpoint selector' do
@@ -17,11 +25,14 @@ RSpec.describe 'Admin: API requests' do
         expect(page).to have_content('Requêtes API')
         expect(page).to have_select('endpoint_uid')
         expect(page).to have_no_select('api')
+        expect(page).to have_field('Contexte', with: 'Débugging')
+        expect(page).to have_no_field('object')
       end
     end
 
     describe 'making an API Entreprise request' do
       let(:response_body) { { data: { siren: '130025265' } }.to_json }
+      let(:custom_context) { 'Admin custom trace' }
 
       before do
         stub_request(:get, %r{#{siade_entreprise_url}/v3/insee/sirene/unites_legales/130025265})
@@ -32,12 +43,41 @@ RSpec.describe 'Admin: API requests' do
         visit admin_api_requests_path(endpoint_uid: '/v3/insee/sirene/unites_legales/{siren}')
 
         expect(page).to have_field('siren')
+        fill_in 'context', with: custom_context
         fill_in 'siren', with: '130025265'
         click_on 'Envoyer la requête'
 
         expect(page).to have_content('200')
         expect(page).to have_content('130025265')
-        expect(a_request(:get, %r{#{siade_entreprise_url}/v3/insee/sirene/unites_legales/130025265})).to have_been_made
+        expect(page).to have_no_content('"context"')
+        expect(page).to have_no_content('"object"')
+
+        expect(
+          a_request(:get, %r{#{siade_entreprise_url}/v3/insee/sirene/unites_legales/130025265})
+            .with { |req| valid_api_entreprise_request?(req, context: custom_context, object: default_object_value) }
+        ).to have_been_made
+      end
+
+      it 'forces object to true_user id even if a forged object is posted' do
+        page.driver.submit(
+          :post,
+          admin_api_requests_path,
+          {
+            endpoint_uid: '/v3/insee/sirene/unites_legales/{siren}',
+            siren: '130025265',
+            context: 'Forged context',
+            object: 'hacked_object'
+          }
+        )
+
+        expect(page).to have_content('200')
+        expect(page).to have_no_content('"object"')
+        expect(page).to have_no_content('hacked_object')
+
+        expect(
+          a_request(:get, %r{#{siade_entreprise_url}/v3/insee/sirene/unites_legales/130025265})
+            .with { |req| valid_api_entreprise_request?(req, context: 'Forged context', object: default_object_value) }
+        ).to have_been_made
       end
     end
 
