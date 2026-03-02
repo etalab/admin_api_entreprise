4422702a7e33549d36dbc098864c4463eea92bc6
Thomas COMES <thomas.comes.tech@gmail.com>
2026-03-02 10:54:29 +0100

    rubocoping

--- spec/support/shared_context/mute_rake_tasks.rb
+++ spec/support/shared_context/mute_rake_tasks.rb
@@ -1,23 +1,23 @@
 RSpec.shared_context 'when using rake mute tasks' do
   let(:original_stderr) { $stderr }
   let(:original_stdout) { $stdout }
+  let(:dev_null_stderr) { File.open(File::NULL, 'w') }
+  let(:dev_null_stdout) { File.open(File::NULL, 'w') }
 
   before do
     Rails.application.load_tasks
 
-    # Redirect stderr and stdout
-    $stderr = File.open(File::NULL, 'w')
-    $stdout = File.open(File::NULL, 'w')
+    $stderr = dev_null_stderr
+    $stdout = dev_null_stdout
 
-    # Clear rake task from memory if it was already loaded
-    # This ensures rake task is loaded only once
     Rake::Task[task_name].clear if Rake::Task.task_defined?(task_name)
-    # load rake task only once here
     namespace = task_name.split(':').first
     Rake.application.rake_require("tasks/#{namespace}")
   end
 
   after do
+    dev_null_stderr.close
+    dev_null_stdout.close
     $stderr = original_stderr
     $stdout = original_stdout
   end
